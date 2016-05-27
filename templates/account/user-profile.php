a542ec696f51dcb74455d8a1d466cf0d5abc7688
dakachi <david87dang@gmail.com>
2016-05-27 17:42:13 +0700

    update layout edit profile, update profile

--- templates/account/user-profile.php
+++ templates/account/user-profile.php
@@ -26,39 +26,35 @@ $user = new ME_User($current_user);
 			<div class="me-col-md-9">
 				<div class="marketengine-profile-info">
 					<div class="me-row">
-
-								<div class="me-col-md-6">
-									<div class="marketengine-text-field">
-										<label class="text"><?php _e("First name", "enginethemes");?></label>
-										<p>Text</p>
-									</div>
-								</div>
-								<div class="me-col-md-6">
-									<div class="marketengine-text-field">
-										<label class="text"><?php _e("Last name", "enginethemes");?></label>
-										<p>Admin</p>
-									</div>
-								</div>
-							</div>
+						<div class="me-col-md-6">
 							<div class="marketengine-text-field">
-								<label class="text"><?php _e("Display name", "enginethemes");?></label>
-								<p>Text	Admin</p>
+								<label class="text"><?php _e("First name", "enginethemes");?></label>
+								<p>Text</p>
 							</div>
+						</div>
+						<div class="me-col-md-6">
 							<div class="marketengine-text-field">
-								<label class="text"><?php _e("Username", "enginethemes");?></label>
-								<p>admin</p>
+								<label class="text"><?php _e("Last name", "enginethemes");?></label>
+								<p>Admin</p>
 							</div>
-							<div class="marketengine-text-field">
-								<label class="text"><?php _e("Email", "enginethemes");?></label>
-								<p>admin@enginethemes.com</p>
-							</div>
-							<div class="marketengine-text-field me-no-margin-bottom">
-								<label class="text"><?php _e("Location", "enginethemes");?></label>
-								<p>Vietnamese</p>
-							</div>
-
-
-
+						</div>
+					</div>
+					<div class="marketengine-text-field">
+						<label class="text"><?php _e("Display name", "enginethemes");?></label>
+						<p>Text	Admin</p>
+					</div>
+					<div class="marketengine-text-field">
+						<label class="text"><?php _e("Username", "enginethemes");?></label>
+						<p>admin</p>
+					</div>
+					<div class="marketengine-text-field">
+						<label class="text"><?php _e("Email", "enginethemes");?></label>
+						<p>admin@enginethemes.com</p>
+					</div>
+					<div class="marketengine-text-field me-no-margin-bottom">
+						<label class="text"><?php _e("Location", "enginethemes");?></label>
+						<p>Vietnamese</p>
+					</div>
 				</div>
 				<div class="marketengine-text-field edit-profile">
 					<a href="<?php echo me_get_endpoint_url('edit-profile'); ?>" class="marketengine-btn"><?php _e("EditProfile", "enginethemes");?></a>
