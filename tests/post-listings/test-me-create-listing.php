6843679f48549e5f96f265ac2c79bc45077c24d9
dakachi <david87dang@gmail.com>
2016-06-08 14:16:40 +0700

    update test case reset pass, create listing

--- tests/post-listings/test-me-create-listing.php
+++ tests/post-listings/test-me-create-listing.php
@@ -20,6 +20,54 @@ class Tests_ME_Create_Listing extends WP_UnitTestCase {
         $this->assertEquals(new WP_Error('post_title', 'The post title field is required.'), $p1);
     }
 
+    public function test_create_listing_with_empty_post_content() {
+        $listing_data = array(
+            'post_title' => 'Listing A',
+            'post_content' => '',
+            'listing_type' => 'contact',
+            'meta_input' => array(
+                'price' => '22',
+            ),
+            'tax_input' => array(
+                'listing_category' => '1',
+            )
+        );
+        $p1 = ME_Listing_Handle::insert($listing_data);
+        $this->assertEquals(new WP_Error('post_content', 'The post content field is required.'), $p1);
+    }
+
+    public function test_create_listing_with_empty_listing_contact() {
+        $listing_data = array(
+            'post_title' => 'Listing A',
+            'post_content' => 'Sample content',
+            'listing_type' => 'contacts',
+            'meta_input' => array(
+                'price' => '22',
+            ),
+            'tax_input' => array(
+                'listing_category' => '1',
+            )
+        );
+        $p1 = ME_Listing_Handle::insert($listing_data);
+        $this->assertEquals(new WP_Error('listing_type', 'The selected listing type is invalid.'), $p1);
+    }
+
+    public function test_create_listing_with_invalid_price() {
+        $listing_data = array(
+            'post_title' => 'Listing A',
+            'post_content' => 'Sample content',
+            'listing_type' => 'contacts',
+            'meta_input' => array(
+                'me_price' => '222a',
+            ),
+            'tax_input' => array(
+                'listing_category' => '1',
+            )
+        );
+        $p1 = ME_Listing_Handle::insert($listing_data);
+        $this->assertEquals(new WP_Error('listing_type', 'The selected listing type is invalid.'), $p1);
+    }
+
 
     // TODO: view test_media_handle_upload_sets_post_excerpt in tests/media.php
 }
