c41ed813ea86e49be6258644b67afd78b17388b4
dakachi <david87dang@gmail.com>
2016-06-14 13:34:59 +0700

    update page submit listing

--- templates/post-listing/listing-information.php
+++ templates/post-listing/listing-information.php
@@ -1,19 +1,21 @@
 <div class="marketengine-group-field">
 	<div class="marketengine-input-field">
 	    <label class="text"><?php _e("Listing title", "enginethemes");?></label>
-	    <input type="text" name="listing_title" value="" class="required">
+	    <input type="text" name="listing_title" value="<?php if (!empty($_POST['listing_title'])) {echo esc_attr($_POST['listing_title']);}?>" class="required">
 	</div>
 </div>
 <div class="marketengine-group-field">
 	<div class="marketengine-textarea-field">
 		<label class="text"><?php _e("Description", "enginethemes");?></label>
-		<?php wp_editor(
-			    '',
+		<?php 
+			$listing_content = !empty($_POST['listing_content']) ? $_POST['listing_content'] : '';
+			wp_editor(
+			    $listing_content,
 			    'listing_content',
 			    array(
-			        'quicktags' => false,
+			        'quicktags' => true,
 			        'media_buttons' => false,
-			        'wpautop' => false,
+			        'wpautop' => true,
 			        'teeny' => true,
 			    )
 			);
--- templates/post-listing/listing-tags.php
+++ templates/post-listing/listing-tags.php
@@ -1,7 +1,7 @@
 <div class="marketengine-group-field">
 	<div class="marketengine-input-field">
 	    <label class="text"><?php _e("Tags", "enginethemes");?>&nbsp;<small><?php _e("(optional)", "enginethemes");?></small></label>
-	    <input type="text" name="lising_title">
+	    <input type="text" name="listing_tags">
 	    <div class="marketengine-tags">
 	    	<span class="me-label-tags">Jambo<i class="icon-cancel"></i></span>
 	    	<span class="me-label-tags">Movies<i class="icon-cancel"></i></span>
