09ae8417abed7873b926058270face2be52515f1
thoitranyw <thoitq@youngworld.vn>
2016-06-16 17:18:43 +0700

    style autocomplete tags

--- assets/js/tag-box.js
+++ assets/js/tag-box.js
@@ -81,7 +81,7 @@ var tagBox, array_unique_noempty;
 
                 // If tags editing isn't disabled, create the X button.
                 if ( ! disabled ) {
-                    xbutton = $( '<a id="' + id + '-check-num-' + key + '" class="ntdelbutton" tabindex="0">X</a>' );
+                    xbutton = $( '<i id="' + id + '-check-num-' + key + '" class="ntdelbutton icon-cancel" tabindex="0"></i>' );
 
                     xbutton.on( 'click keypress', function( e ) {
                         // Trigger function if pressed Enter - keyboard navigation
