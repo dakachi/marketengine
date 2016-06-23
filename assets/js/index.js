7bf481492675dbe380f4965b46541fdc8b26a38c
dakachi <david87dang@gmail.com>
2016-06-23 15:17:51 +0700

    update listing gallery slider

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,15 @@
 /* global me_globals.ajaxurl, wpAjax*/
 (function($) {
 	$('.me-list-thumbs').meSliderThumbs();
+	var magnificInstance = true;
+	var magnificItem = 0;
+	
+	$('.me-fancybox').on('click', function(ev) {
+		ev.preventDefault();
+		var target = ev.currentTarget;
+		var medium_img = $(target).attr('medium-img');  
+		$('.me-large-fancybox').find('img').attr('src', medium_img);
+		magnificItem = $(target).parent('li').index();
+		magnificInstance = false;
+	});
 })(jQuery);
