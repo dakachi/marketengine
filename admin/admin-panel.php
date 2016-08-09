4748929a62ff17d17c4b8160f914f6eb081ae791
dakachi <david87dang@gmail.com>
2016-08-09 15:52:44 +0700

    update admin option

--- admin/admin-panel.php
+++ admin/admin-panel.php
@@ -1,9 +1,38 @@
 <?php
-function marketengine_option_view () {
-	include 'option-view.php';
+function marketengine_option_view() {
+
+    marketengine_option_header();
+    //include 'option-view.php';
+    $tabs = array(
+        'general-settings'      => array(
+            'title' => __("General", "enginethemes"),
+            'slug'  => 'general-settings',
+            'menus' => array(),
+        ),
+        'authenticate-settings' => array(
+            'title' => __("Authentication", "enginethemes"),
+            'slug'  => 'authenticate-settings',
+            'menus' => array(),
+        ),
+    );
+    echo '<div class="marketengine-tabs me-tabs-general">';
+
+    echo '<ul class="me-tabs ">';
+    foreach ($tabs as $tab) {
+        echo '<li class="active"><span>'.$tab['title'].'</span></li>';
+    }
+    echo '</ul>';
+
+    foreach ($tabs as $tab) {
+        $tab = new AE_Tab($tab);
+        $tab->render();
+    }
+
+    echo '</div>';
+    marketengine_option_footer();
 }
 
-/** 
+/**
  * Add marketengine admin menu
  */
 function marketengine_option_menu() {
@@ -20,13 +49,12 @@ function marketengine_option_menu() {
 add_action('admin_menu', 'marketengine_option_menu');
 
 function marketengine_load_admin_option_script_css() {
-	wp_register_style( 'marketengine-font-icon',  ME_PLUGIN_URL . '/assets/admin/jquery.mCustomScrollbar.min.css', array(), '1.0');
-	wp_enqueue_style( 'me-option-css', ME_PLUGIN_URL . '/assets/admin/marketengine-admin.css');
+    wp_register_style('marketengine-font-icon', ME_PLUGIN_URL . '/assets/admin/jquery.mCustomScrollbar.min.css', array(), '1.0');
+    wp_enqueue_style('me-option-css', ME_PLUGIN_URL . '/assets/admin/marketengine-admin.css');
 
-	wp_enqueue_script( 'jquery-scrollbar', ME_PLUGIN_URL . '/assets/admin/jquery.mCustomScrollbar.min.js', array('jquery'), '1.0', true );
-	wp_enqueue_script( 'marketengine-option', ME_PLUGIN_URL . '/assets/admin/script-admin.js', array('jquery', 'jquery-scrollbar'), '1.0', true );
+    wp_enqueue_script('jquery-scrollbar', ME_PLUGIN_URL . '/assets/admin/jquery.mCustomScrollbar.min.js', array('jquery'), '1.0', true);
+    wp_enqueue_script('marketengine-option', ME_PLUGIN_URL . '/assets/admin/script-admin.js', array('jquery', 'jquery-scrollbar'), '1.0', true);
 
-	
 }
 add_action('admin_enqueue_scripts', 'marketengine_load_admin_option_script_css');
 
@@ -34,13 +62,13 @@ add_action('admin_enqueue_scripts', 'marketengine_load_admin_option_script_css')
  * Render marketengine admin option header
  * @since 1.0
  */
-function marketengine_option_header(){
-?>
+function marketengine_option_header() {
+    ?>
 
 <div class="marketengine-admin">
 	<div class="me-header">
-		<span class="pull-left"><?php _e("MARKETENGINE", "enginethemes"); ?></span>
-		<span class="pull-right"><?php _e("Power by", "enginethemes"); ?> <a href="https://www.enginethemes.com/">EngineThemes</a></span>
+		<span class="pull-left"><?php _e("MARKETENGINE", "enginethemes");?></span>
+		<span class="pull-right"><?php _e("Power by", "enginethemes");?> <a href="https://www.enginethemes.com/">EngineThemes</a></span>
 	</div>
 	<div class="me-body">
 <?php
@@ -50,10 +78,10 @@ function marketengine_option_header(){
  * Render marketengine admin option footer
  * @since 1.0
  */
-function marketengine_option_footer(){
-?>
+function marketengine_option_footer() {
+    ?>
 	</div>
 </div>
-	
+
 <?php
 }
--- admin/fields/ae-section.php
+++ admin/fields/ae-section.php
@@ -4,52 +4,55 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-
-
 class AE_Tab {
-	protected $_controls;
+    protected $_controls;
     public function __construct() {
 
     }
 
     public function render() {
-    	echo '<div class="me-tab">';
-    	foreach ($this->_controls as $key => $control) {
-    		$control->render();
-    	}
-    	echo '</div>';
+        echo '<div class="me-tab">';
+        if(!$this->_controls) return;
+        foreach ($this->_controls as $key => $control) {
+            $control->render();
+        }
+        echo '</div>';
     }
 }
 
 class AE_Section {
-	protected $_controls;
+    protected $_controls;
     public function __construct() {
 
     }
 
     public function render() {
-    	echo '<div class="me-section">';
-    	foreach ($this->_controls as $key => $control) {
-    		$control->render();
-    	}
-    	echo '</div>';
+        echo '<div class="me-section">';
+        if(!$this->_controls) return;
+        foreach ($this->_controls as $key => $control) {
+            $control->render();
+        }
+        echo '</div>';
     }
 }
 
 class AE_Group {
-	protected $_title;
-	protected $_description;
-	protected $_fields;
-    public function __construct() {
+    protected $_title;
+    protected $_description;
+    protected $_fields;
+    /**
+     *
+     */
+    public function __construct($args, $option) {
 
     }
 
     public function render() {
-    	echo '<div class="me-group-field">';
-    	foreach ($this->_fields as $key => $field) {
-    		$field->render();
-    	}
-    	echo '</div>';
+        echo '<div class="me-group-field">';
+        foreach ($this->_fields as $key => $field) {
+            $field->render();
+        }
+        echo '</div>';
     }
 }
 
@@ -58,20 +61,53 @@ abstract class AE_Input {
     protected $_type;
     protected $_label;
     protected $_description;
+    protected $_container;
+    protected $_value;
+
+    protected $_options;
     abstract public function render();
+
+    protected function label() {
+        if (!empty($this->_label)) {
+            echo '<label class="me-title">' . $this->_label . '</label>';
+        }
+    }
+
+    protected function description() {
+        if (!empty($this->_description)) {
+            echo '<span class="me-subtitle">' . $this->_description . '</span>';
+        }
+    }
+
+    protected function get_value() {
+        if (!$this->_container || !$this->_options) {
+            return '';
+        }
+        $parent = $this->_container;
+        $options = $this->_options;
+        $option_name = $this->_name;
+
+        return $options->$parent->$option_name;
+    }
 }
 
 class AE_Textbox extends AE_Input {
-    public function __construct() {
+    public function __construct($args, $options, $parent) {
+        $args = wp_parse_args($args, array('name' => 'option_name', 'description' => '', 'label' => ''));
 
+        $this->_type        = 'textbox';
+        $this->_name        = $args['name'];
+        $this->_label       = $args['label'];
+        $this->_description = $args['description'];
+        $this->_container   = $parent;
+
+        $this->_options = $options;
     }
+
     public function render() {
-    ?>
-    	<div class="me-group-field">
-			<label class="me-title"><?php echo $this->_label; ?></label>
-			<span class="me-subtitle"><?php echo $this->_description; ?></span>
-			<input type="text" class="input-field" value="<?php echo $this->value; ?>" />
-		</div>
-    <?php
+        $this->label();
+        $this->description();
+        echo '<input type="text" class="input-field" value="' . $this->get_value() . '" />';
+
     }
 }
