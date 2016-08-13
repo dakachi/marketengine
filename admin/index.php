653dccf9f8d07565ccec25c4580b2361e579b134
dakachi <david87dang@gmail.com>
2016-08-13 10:59:30 +0700

    fix admin panel

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once ('/fields/ae-section.php');
+require_once ('./fields/ae-section.php');
 
-require_once ( '/admin-panel.php');
-require_once ('/admin-options.php');
+require_once ( './admin-panel.php');
+require_once ('./admin-options.php');
