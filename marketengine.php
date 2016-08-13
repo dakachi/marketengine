877447f91bb13d94a8c5179ff0427ebf0ef6ae85
dakachi <david87dang@gmail.com>
2016-08-13 10:58:39 +0700

    fix admin panel

--- marketengine.php
+++ marketengine.php
@@ -80,9 +80,9 @@ if (!class_exists('MarketEngine')):
         }
 
         private function include_files() {
-            require_once '/includes/class-me-autoloader.php';
+            require_once ME_PLUGIN_PATH .'/includes/class-me-autoloader.php';
 
-            require_once '/admin/index.php';
+            require_once ME_PLUGIN_PATH .'/admin/index.php';
 
             require_once ME_PLUGIN_PATH . '/includes/class-me-install.php';
             require_once ME_PLUGIN_PATH . '/includes/class-me-session.php';
