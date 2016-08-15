dd78705e75090085352842e03d367a5f21e4b5b1
dakachi <david87dang@gmail.com>
2016-08-15 13:08:53 +0700

    add class autoloader for admin field

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once (ME_PLUGIN_PATH . '/admin/fields/ae-section.php');
+require_once (ME_PLUGIN_PATH . '/admin/fields/class-auto-loader.php');
 
 require_once ( ME_PLUGIN_PATH . '/admin/admin-panel.php');
 require_once (ME_PLUGIN_PATH . '/admin/admin-options.php');
