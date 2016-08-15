0804cf108be6f1dac6587464b1b6e865dab701b2
dakachi <david87dang@gmail.com>
2016-08-15 17:03:46 +0700

    update me query

--- includes/class-me-query.php
+++ includes/class-me-query.php
@@ -14,60 +14,64 @@ function me_init() {
 // todo: filter listing by listing type
 
 function me_filter_listing_query($query) {
-	// We only want to affect the main query
-	if ( ! $query->is_main_query() ) {
-		return;
-	}
+    // We only want to affect the main query
+    if (!$query->is_main_query()) {
+        return;
+    }
 
-	if(! $query->is_post_type_archive( 'listing' ) && ! $query->is_tax( get_object_taxonomies( 'listing' ) )) {
-		return $query;
-	}
-	
-	$query = me_filter_price_query($query);
-	$query = me_sort_listing_query($query);
-	
-	return $query;
+    if (!$query->is_post_type_archive('listing') && !$query->is_tax(get_object_taxonomies('listing'))) {
+        return $query;
+    }
+
+    $query = me_filter_price_query($query);
+    $query = me_sort_listing_query($query);
+
+    return $query;
 }
-add_filter( 'pre_get_posts', 'me_filter_listing_query');
+add_filter('pre_get_posts', 'me_filter_listing_query');
 
 function me_filter_price_query($query) {
-	$min_price = $_GET['price-min'];
-	$max_price = $_GET['price-max'];
-	$query->set('meta_query', array(
-		array(
-			'key'     => 'listing_price',
-			'value'   => array( $min_price, $max_price ),
-			'type'    => 'numeric',
-			'compare' => 'BETWEEN',
-		)
-	));
-	$query->set('meta_key', 'listing_price');
-	
-	return $query;
+    if (!empty($_GET['price-min']) && !empty($_GET['price-max'])) {
+        $min_price = $_GET['price-min'];
+        $max_price = $_GET['price-max'];
+        $query->set('meta_query', array(
+            array(
+                'key'     => 'listing_price',
+                'value'   => array($min_price, $max_price),
+                'type'    => 'numeric',
+                'compare' => 'BETWEEN',
+            ),
+        ));
+        $query->set('meta_key', 'listing_price');
+    }
+    return $query;
 }
 
 function me_sort_listing_query($query) {
-	if(empty($_GET['orderby'])) return $query;
-	switch ($_GET['orderby']) {
-		case 'date':
-			$query->set('orderby', 'date');
-			break;
+    if (empty($_GET['orderby'])) {
+        return $query;
+    }
 
-		case 'price' : 
-			$query->set('orderby', 'meta_value_num');
-			$query->set('order', 'asc');	
-			break;
+    switch ($_GET['orderby']) {
+    case 'date':
+        $query->set('orderby', 'date');
+        break;
 
-		case 'price-desc' : 
-			$query->set('orderby', 'meta_value_num');
-			$query->set('order', 'desc');	
-			break;
-		case 'rating' : 
-			$query->set('meta_key', '_me_rating');
-			$query->set('orderby', 'meta_value_num');
-			$query->set('order', 'desc');	
-		default:
-			break;
-	}	
-	return $query;
+    case 'price':
+        $query->set('orderby', 'meta_value_num');
+        $query->set('order', 'asc');
+        break;
+
+    case 'price-desc':
+        $query->set('orderby', 'meta_value_num');
+        $query->set('order', 'desc');
+        break;
+    case 'rating':
+        $query->set('meta_key', '_me_rating');
+        $query->set('orderby', 'meta_value_num');
+        $query->set('order', 'desc');
+    default:
+        break;
+    }
+    return $query;
 }
