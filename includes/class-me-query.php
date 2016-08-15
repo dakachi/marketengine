7d4a631b42c87d21eefa97344025162f26adf262
dakachi <david87dang@gmail.com>
2016-08-15 16:16:45 +0700

    add filter product by price

--- includes/class-me-query.php
+++ includes/class-me-query.php
@@ -18,3 +18,40 @@ function me_init() {
 // todo: query listing order by date, rating, price
 // todo: filter listing by price
 // todo: filter listing by listing type
+
+function me_filter_listing_query($query) {
+	// We only want to affect the main query
+	if ( ! $query->is_main_query() ) {
+		return;
+	}
+
+	if(! $query->is_post_type_archive( 'listing' ) && ! $query->is_tax( get_object_taxonomies( 'listing' ) )) {
+		return $query;
+	}
+	
+	$query = me_filter_price_query($query);
+	$query = me_sort_listing_query($query);
+	
+	return $query;
+}
+add_filter( 'pre_get_posts', 'me_filter_listing_query');
+
+function me_filter_price_query($query) {
+	$min_price = $_GET['price-min'];
+	$max_price = $_GET['price-max'];
+	$query->set('meta_query', array(
+		array(
+			'key'     => 'listing_price',
+			'value'   => array( $min_price, $max_price ),
+			'type'    => 'numeric',
+			'compare' => 'BETWEEN',
+		)
+	));
+	$query->set('meta_key', 'listing_price');
+	
+	return $query;
+}
+
+function me_sort_listing_query($query) {
+	return $query;
+}
