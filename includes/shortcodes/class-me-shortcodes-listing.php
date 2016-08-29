5f80938ee2353dd97742b99c897f63fb774bb328
dakachi <david87dang@gmail.com>
2016-08-29 18:02:27 +0700

    add template confirm payment

--- includes/shortcodes/class-me-shortcodes-listing.php
+++ includes/shortcodes/class-me-shortcodes-listing.php
@@ -4,6 +4,7 @@ class ME_Shortcodes_Listing {
         add_shortcode('me_post_listing_form', array(__CLASS__, 'post_listing_form'));
         add_shortcode('me_listings', array(__CLASS__, 'the_listing'));
         add_shortcode('me_checkout_form', array(__CLASS__, 'checkout_form'));
+        add_shortcode('me_confirm_order', array(__CLASS__, 'confirm_order'));
     }
     public static function post_listing_form() {
     	ob_start();
@@ -19,6 +20,13 @@ class ME_Shortcodes_Listing {
     public static function checkout_form() {
         ob_start();
         me_get_template_part('checkout/checkout');
+        $content = ob_get_clean();
+        return $content;
+    }
+
+    public static function confirm_order() {
+        ob_start();
+        me_get_template_part('checkout/confirm');
         $content = ob_get_clean();
         return $content;
     }
