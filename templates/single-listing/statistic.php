c69de0eee95b291bd9faba9f10d2a752c15cac07
thoitranyw <thoitq@youngworld.vn>
2016-08-30 14:19:38 +0700

    update html template statistic.php

--- templates/single-listing/statistic.php
+++ templates/single-listing/statistic.php
@@ -8,24 +8,33 @@ global $post;
 $listing = new ME_Listing($post);
 $order_count = $listing->get_order_count();
 $review_count = $listing->get_review_count();
+$listing_type = $listing->get_listing_type();
 ?>
 
 <?php do_action('marketengine_before_single_listing_statistic'); ?>
+	<?php if('contact' === $listing_type) : ?>
+		<div class="me-rating-contact">
+			<span class="me-count-contact">
+			<?php printf(_n("%d Contact", "%d Contacts", $review_count,"enginethemes"),$review_count ); ?>
+		</span>|
+		</div>
+	<?php endif; ?>
 
-<div class="me-rating-contact">
-	<div class="me-rating">
-		<i class="icon-font star-on-png"></i>
-		<i class="icon-font star-on-png"></i>
-		<i class="icon-font star-on-png"></i>
-		<i class="icon-font star-half-png"></i>
-		<i class="icon-font star-off-png"></i>
-	</div>
-	<span class="me-count-contact">
-		<?php printf(_n("%d Contact", "%d Contacts", $review_count,"enginethemes"),$review_count ); ?>
-	</span>|
-	<span class="me-count-review">
-		<?php printf(_n("%d Review", "%d Reviews", $review_count,"enginethemes"),$review_count ); ?>
-	</span>
-</div>
-
+	<?php if('purchasion' == $listing_type) : ?>
+		<div class="me-rating-purchases">
+			<div class="me-rating">
+				<i class="icon-me-star"></i>
+				<i class="icon-me-star"></i>
+				<i class="icon-me-star"></i>
+				<i class="icon-me-star"></i>
+				<i class="icon-me-star"></i>
+			</div>
+			<span class="me-count-purchases">
+				<?php printf(_n("%d Purchase", "%d Purchases", $review_count,"enginethemes"),$review_count ); ?>
+			</span>
+			<span class="me-count-review">
+				<?php printf(_n("%d Review", "%d Reviews", $review_count,"enginethemes"),$review_count ); ?>
+			</span>
+		</div>	
+	<?php endif; ?>
 <?php do_action('marketengine_after_single_listing_statistic'); ?>
