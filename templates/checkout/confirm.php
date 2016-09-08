448b2898e7b2b8438160b01dd87b0287f0e3267a
dakachi <david87dang@gmail.com>
2016-09-08 16:43:10 +0700

    update confirm order

--- templates/checkout/confirm.php
+++ templates/checkout/confirm.php
@@ -3,7 +3,6 @@ $order_id = get_query_var( 'order-id' );
 $order = new ME_Order($order_id);
 $paypal = ME_Paypal_Simple::instance();
 $paypal->complete_payment($_REQUEST);
-
 ?>
 <div class="marketengine">
 	<div class="me-payment-complete">
@@ -36,55 +35,3 @@ $paypal->complete_payment($_REQUEST);
 
 	</div>
 </div>
-<?php
-/*
-Array
-(
-	[mc_gross] => 4000.00
-	[protection_eligibility] => Eligible
-	[address_status] => confirmed
-	[item_number1] => 132
-	[payer_id] => UADMHTRMVRHTQ
-	[tax] => 0.00
-	[address_street] => 1 Main St
-	[payment_date] => 21:37:11 Aug 29, 2016 PDT
-	[payment_status] => Completed
-	[charset] => utf-8
-	[address_zip] => 95131
-	[mc_shipping] => 0.00
-	[mc_handling] => 0.00
-	[first_name] => ledd
-	[mc_fee] => 116.30
-	[address_country_code] => US
-	[address_name] => ledd dakac
-	[notify_version] => 3.8
-	[custom] => {\"order_id\":404}
-	[payer_status] => verified
-	[business] => dinhle1987-biz@yahoo.com
-	[address_country] => United States
-	[num_cart_items] => 1
-	[mc_handling1] => 0.00
-	[address_city] => San Jose
-	[payer_email] => dinhle1987-pers2@yahoo.com
-	[verify_sign] => AWfYvpOYY9QGK6LMTcBhquaTPxFyALShAu0huejjZ8Qh9IUoK6mfw26.
-	[mc_shipping1] => 0.00
-	[tax1] => 0.00
-	[txn_id] => 6B91747228835915A
-	[payment_type] => instant
-	[last_name] => dakac
-	[item_name1] => Selling Listing 1
-	[address_state] => CA
-	[receiver_email] => dinhle1987-biz@yahoo.com
-	[payment_fee] => 116.30
-	[quantity1] => 2
-	[receiver_id] => PTRV3WCVA7PP2
-	[txn_type] => cart
-	[mc_gross_1] => 4000.00
-	[mc_currency] => USD
-	[residence_country] => US
-	[test_ipn] => 1
-	[transaction_subject] =>
-	[payment_gross] => 4000.00
-	[auth] => AO8EvUSOmjOHrgZrpV9DYI2drRBTM.ak4dwt9yFB1ppVM7rWgCqxhv0M6i0Q4ih6K8LgkMrmpFRCtiAArGftI1A
-)
- */
