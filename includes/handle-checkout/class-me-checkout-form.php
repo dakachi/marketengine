fdf10861ef987dedc8e0071284e26f7b78392e51
dakachi <david87dang@gmail.com>
2016-09-22 14:10:55 +0700

    finish paypal adaptive ipn

--- includes/handle-checkout/class-me-checkout-form.php
+++ includes/handle-checkout/class-me-checkout-form.php
@@ -48,7 +48,7 @@ class ME_Checkout_Form
         if (!empty($_GET['me-payment'])) {
             $request = sanitize_text_field( strtolower($_GET['me-payment']) );
             do_action('marketegine_' . $request , $_REQUEST);
-            update_option( 'handle', 'marketegine_' . $request );
+            update_option( 'handle', 'ipn_posted_to_marketegine_' . $request );
             // $paypal = ME_PPAdaptive_Request::instance();
             // $paypal->complete_payment($_REQUEST);            
         }
