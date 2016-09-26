587c9b6e46f3f0f38a1e11c3f82dfa09c7c28442
dakachi <david87dang@gmail.com>
2016-09-26 16:19:20 +0700

    add function me update message

--- includes/me-conversation-functions.php
+++ includes/me-conversation-functions.php
@@ -4,155 +4,300 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-function me_insert_message($message_arr, $wp_error = false){
-	global $wpdb;
- 
+function me_insert_message($message_arr, $wp_error = false) {
+    global $wpdb;
+
     $user_id = get_current_user_id();
 
     $defaults = array(
-        'sender' => $user_id,
-        'receiver' => '',
-        'post_content' => '',
+        'sender'                => $user_id,
+        'receiver'              => '',
+        'post_content'          => '',
         'post_content_filtered' => '',
-        'post_title' => '',
-        'post_status' => 'draft',
-        'post_type' => 'post',
-        'comment_status' => '',
-        'post_password' => '',
-        'post_parent' => 0,
-        'guid' => ''
+        'post_title'            => '',
+        'post_status'           => 'draft',
+        'post_type'             => 'post',
+        'post_password'         => '',
+        'post_parent'           => 0,
+        'guid'                  => '',
     );
 
     $message_arr = wp_parse_args($message_arr, $defaults);
 
-    $message_arr = sanitize_post( $message_arr, 'db' );
+    $message_arr = sanitize_post($message_arr, 'db');
     // Are we updating or creating?
-	$message_ID = 0;
-	$update = false;
-	$guid = $message_arr['guid'];
+    $message_ID = 0;
+    $update     = false;
+    $guid       = $message_arr['guid'];
 
-	if ( ! empty( $message_arr['ID'] ) ) {
-		$update = true;
+    if (!empty($message_arr['ID'])) {
+        $update = true;
 
-		// Get the post ID and GUID.
-		$message_ID = $message_arr['ID'];
-		$message_before = me_get_message( $message_ID );
-		if ( is_null( $message_before ) ) {
-			if ( $wp_error ) {
-				return new WP_Error( 'invalid_post', __( 'Invalid message ID.' ) );
-			}
-			return 0;
-		}
+        // Get the post ID and GUID.
+        $message_ID     = $message_arr['ID'];
+        $message_before = me_get_message($message_ID);
+        if (is_null($message_before)) {
+            if ($wp_error) {
+                return new WP_Error('invalid_post', __('Invalid message ID.'));
+            }
+            return 0;
+        }
 
-		$guid = me_get_message_field( 'guid', $message_ID );
-		$previous_status = me_get_message_field('message_status', $message_ID ); // get_post_field
-	} else {
-		$previous_status = 'new';
-	}
+        $guid            = me_get_message_field('guid', $message_ID);
+        $previous_status = me_get_message_field('message_status', $message_ID); // get_post_field
+    } else {
+        $previous_status = 'new';
+    }
 
-	$post_type = empty( $message_arr['post_type'] ) ? 'message' : $message_arr['post_type'];
+    $post_type = empty($message_arr['post_type']) ? 'message' : $message_arr['post_type'];
 
-	$post_title = $message_arr['post_title'];
-	$post_content = $message_arr['post_content'];
-	$post_excerpt = $message_arr['post_excerpt'];
-	if ( isset( $message_arr['post_name'] ) ) {
-		$post_name = $message_arr['post_name'];
-	} elseif ( $update ) {
-		// For an update, don't modify the post_name if it wasn't supplied as an argument.
-		$post_name = $post_before->post_name;
-	}
+    $post_title   = $message_arr['post_title'];
+    $post_content = $message_arr['post_content'];
+    $post_excerpt = $message_arr['post_excerpt'];
+    if (isset($message_arr['post_name'])) {
+        $post_name = $message_arr['post_name'];
+    } elseif ($update) {
+        // For an update, don't modify the post_name if it wasn't supplied as an argument.
+        $post_name = $message_before->post_name;
+    }
 
-	$maybe_empty = 'attachment' !== $post_type
-		&& ! $post_content && ! $post_title && ! $post_excerpt;
+    $maybe_empty = 'attachment' !== $post_type
+    && !$post_content && !$post_title && !$post_excerpt;
 
-	/**
-	 * Filters whether the message should be considered "empty".
-	 *
-	 * Returning a truthy value to the filter will effectively short-circuit
-	 * the new post being inserted, returning 0. If $wp_error is true, a WP_Error
-	 * will be returned instead.
-	 *
-	 * @since 1.0
-	 *
-	 * @param bool  $maybe_empty Whether the post should be considered "empty".
-	 * @param array $message_arr     Array of post data.
-	 */
-	if ( apply_filters( 'me_insert_message_empty_content', $maybe_empty, $message_arr ) ) {
-		if ( $wp_error ) {
-			return new WP_Error( 'empty_content', __( 'Content, title, and excerpt are empty.' ) );
-		} else {
-			return 0;
-		}
-	}
+    /**
+     * Filters whether the message should be considered "empty".
+     *
+     * Returning a truthy value to the filter will effectively short-circuit
+     * the new post being inserted, returning 0. If $wp_error is true, a WP_Error
+     * will be returned instead.
+     *
+     * @since 1.0
+     *
+     * @param bool  $maybe_empty Whether the post should be considered "empty".
+     * @param array $message_arr     Array of post data.
+     */
+    if (apply_filters('me_insert_message_empty_content', $maybe_empty, $message_arr)) {
+        if ($wp_error) {
+            return new WP_Error('empty_content', __('Content, title, and excerpt are empty.'));
+        } else {
+            return 0;
+        }
+    }
 
-	if(empty($message_arr['receiver'])) {
-		if ( $wp_error ) {
-			return new WP_Error( 'empty_receiver', __( 'Receiver is empty.' ) );
-		} else {
-			return 0;
-		}	
-	}
+    if (empty($message_arr['receiver'])) {
+        if ($wp_error) {
+            return new WP_Error('empty_receiver', __('Receiver is empty.'));
+        } else {
+            return 0;
+        }
+    }
 
-	$post_status = empty( $message_arr['post_status'] ) ? 'unread' : $message_arr['post_status'];
-	/*
-	 * Create a valid post name. Drafts and pending posts are allowed to have
-	 * an empty post name.
-	 */
-	if ( empty($post_name) ) {
-		if ( !in_array( $post_status, array( 'draft', 'pending', 'auto-draft' ) ) ) {
-			$post_name = sanitize_title($post_title);
-		} else {
-			$post_name = '';
-		}
-	} else {
-		// On updates, we need to check to see if it's using the old, fixed sanitization context.
-		$check_name = sanitize_title( $post_name, '', 'old-save' );
-		if ( $update && strtolower( urlencode( $post_name ) ) == $check_name && get_post_field( 'post_name', $post_ID ) == $check_name ) {
-			$post_name = $check_name;
-		} else { // new post, or slug has changed.
-			$post_name = sanitize_title($post_name);
-		}
-	}
+    $post_status = empty($message_arr['post_status']) ? 'unread' : $message_arr['post_status'];
+    /*
+     * Create a valid post name. Drafts and pending posts are allowed to have
+     * an empty post name.
+     */
+    if (empty($post_name)) {
+        if (!in_array($post_status, array('draft', 'pending', 'auto-draft'))) {
+            $post_name = sanitize_title($post_title);
+        } else {
+            $post_name = '';
+        }
+    } else {
+        // On updates, we need to check to see if it's using the old, fixed sanitization context.
+        $check_name = sanitize_title($post_name, '', 'old-save');
+        if ($update && strtolower(urlencode($post_name)) == $check_name && get_post_field('post_name', $post_ID) == $check_name) {
+            $post_name = $check_name;
+        } else {
+            // new post, or slug has changed.
+            $post_name = sanitize_title($post_name);
+        }
+    }
 
-	/*
-	 * If the post date is empty (due to having been new or a draft) and status
-	 * is not 'draft' or 'pending', set date to now.
-	 */
-	if ( empty( $message_arr['post_date_gmt'] ) || '0000-00-00 00:00:00' == $message_arr['post_date_gmt'] ) {
-		$post_date = current_time( 'mysql' );
-	} else {
-		$post_date = get_date_from_gmt( $message_arr['post_date_gmt'] );
-	}
-	
-	if ( ! in_array( $post_status, array( 'draft', 'pending', 'auto-draft' ) ) ) {
-		$post_date_gmt = get_gmt_from_date( $post_date );
-	} else {
-		$post_date_gmt = '0000-00-00 00:00:00';
-	}
+    /*
+     * If the post date is empty (due to having been new or a draft) and status
+     * is not 'draft' or 'pending', set date to now.
+     */
+    if (empty($message_arr['post_date_gmt']) || '0000-00-00 00:00:00' == $message_arr['post_date_gmt']) {
+        $post_date = current_time('mysql');
+    } else {
+        $post_date = get_date_from_gmt($message_arr['post_date_gmt']);
+    }
 
-	if ( $update || '0000-00-00 00:00:00' == $post_date ) {
-		$post_modified     = current_time( 'mysql' );
-		$post_modified_gmt = current_time( 'mysql', 1 );
-	} else {
-		$post_modified     = $post_date;
-		$post_modified_gmt = $post_date_gmt;
-	}
+    if (!in_array($post_status, array('draft', 'pending', 'auto-draft'))) {
+        $post_date_gmt = get_gmt_from_date($post_date);
+    } else {
+        $post_date_gmt = '0000-00-00 00:00:00';
+    }
 
-	// These variables are needed by compact() later.
-	$post_content_filtered = $postarr['post_content_filtered'];
-	$sender = $user_id;
-	$receiver = isset( $postarr['receiver'] ) ? $postarr['receiver'] : '';
+    if ($update || '0000-00-00 00:00:00' == $post_date) {
+        $post_modified     = current_time('mysql');
+        $post_modified_gmt = current_time('mysql', 1);
+    } else {
+        $post_modified     = $post_date;
+        $post_modified_gmt = $post_date_gmt;
+    }
 
-	if ( isset( $postarr['post_parent'] ) ) {
-		$post_parent = (int) $postarr['post_parent'];
-	} else {
-		$post_parent = 0;
-	}
+    // These variables are needed by compact() later.
+    $post_content_filtered = $message_arr['post_content_filtered'];
+    $sender                = $user_id;
+    $receiver              = $message_arr['receiver'];
 
+    if (isset($message_arr['post_parent'])) {
+        $post_parent = (int) $message_arr['post_parent'];
+    } else {
+        $post_parent = 0;
+    }
+
+    // Expected_slashed (everything!).
+    $data  = compact('sender', 'receiver', 'post_date', 'post_date_gmt', 'post_content', 'post_content_filtered', 'post_title', 'post_excerpt', 'post_status', 'post_type', 'post_password', 'post_name', 'post_modified', 'post_modified_gmt', 'post_parent', 'guid');
+    $data  = wp_unslash($data);
+    $where = array('ID' => $message_ID);
+
+    $message_table = $wpdb->prefix . 'marketengine_message_item';
+    if ($update) {
+        /**
+         * Fires immediately before an existing post is updated in the database.
+         *
+         * @since 2.5.0
+         *
+         * @param int   $post_ID Post ID.
+         * @param array $data    Array of unslashed post data.
+         */
+        do_action('pre_post_update', $post_ID, $data);
+        if (false === $wpdb->update($message_table, $data, $where)) {
+            if ($wp_error) {
+                return new WP_Error('db_update_error', __('Could not update post in the database'), $wpdb->last_error);
+            } else {
+                return 0;
+            }
+        }
+    } else {
+        // If there is a suggested ID, use it if not already present.
+        if (!empty($import_id)) {
+            $import_id = (int) $import_id;
+            if (!$wpdb->get_var($wpdb->prepare("SELECT ID FROM $message_table WHERE ID = %d", $import_id))) {
+                $data['ID'] = $import_id;
+            }
+        }
+        if (false === $wpdb->insert($message_table, $data)) {
+            if ($wp_error) {
+                return new WP_Error('db_insert_error', __('Could not insert post into the database'), $wpdb->last_error);
+            } else {
+                return 0;
+            }
+        }
+        $post_ID = (int) $wpdb->insert_id;
+
+        // Use the newly generated $post_ID.
+        $where = array('ID' => $post_ID);
+    }
+
+    // TODO: message meta
+    if (!empty($message_arr['meta_input'])) {
+        foreach ($message_arr['meta_input'] as $field => $value) {
+            update_post_meta($post_ID, $field, $value);
+        }
+    }
+
+    $message = me_get_message( $message_ID );
+    if ($update) {
+        /**
+         * Fires once an existing message has been updated.
+         *
+         * @since 1.0
+         *
+         * @param int     $post_ID Post ID.
+         * @param WP_Post $post    Post object.
+         */
+        do_action('edit_message', $message_ID, $message);
+        $message_after = me_get_message($message_ID);
+
+        /**
+         * Fires once an existing message has been updated.
+         *
+         * @since 1.0
+         *
+         * @param int     $message_ID      Message ID.
+         * @param WP_Post $message_after   Message object following the update.
+         * @param WP_Post $message_before  Message object before the update.
+         */
+        do_action('message_updated', $message_ID, $message_after, $message_before);
+    }
+
+    /**
+     * Fires once a message has been saved.
+     *
+     * The dynamic portion of the hook name, `$message->post_type`, refers to
+     * the message type slug.
+     *
+     * @since 1.0
+     *
+     * @param int     $message_ID message ID.
+     * @param WP_Post $message    message object.
+     * @param bool    $update  Whether this is an existing message being updated or not.
+     */
+    do_action("save_message_{$message->post_type}", $message_ID, $message, $update);
+
+    /**
+     * Fires once a message has been saved.
+     *
+     * @since 1.0
+     *
+     * @param int     $message_ID message ID.
+     * @param WP_Post $message    message object.
+     * @param bool    $update  Whether this is an existing message being updated or not.
+     */
+    do_action('save_message', $message_ID, $message, $update);
+
+    /**
+     * Fires once a message has been saved.
+     *
+     * @since 1.0
+     *
+     * @param int     $message_ID message ID.
+     * @param WP_Post $message    message object.
+     * @param bool    $update  Whether this is an existing message being updated or not.
+     */
+    do_action('me_insert_message', $message_ID, $message, $update);
+
+    return $message_ID;
 }
 
-function me_update_message() {
+function me_update_message($message_arr = array(), $wp_error = false ) {
+	if ( is_object($message_arr) ) {
+		// Non-escaped post was passed.
+		$message_arr = get_object_vars($message_arr);
+		$message_arr = wp_slash($message_arr);
+	}
 
+	// First, get all of the original fields.
+	$post = me_get_message($message_arr['ID'], ARRAY_A);
+
+	if ( is_null( $post ) ) {
+		if ( $wp_error )
+			return new WP_Error( 'invalid_post', __( 'Invalid post ID.' ) );
+		return 0;
+	}
+
+	// Escape data pulled from DB.
+	$post = wp_slash($post);
+
+	// Drafts shouldn't be assigned a date unless explicitly done so by the user.
+	if ( isset( $post['post_status'] ) && in_array($post['post_status'], array('draft', 'pending', 'auto-draft')) && empty($message_arr['edit_date']) &&
+			 ('0000-00-00 00:00:00' == $post['post_date_gmt']) )
+		$clear_date = true;
+	else
+		$clear_date = false;
+
+	// Merge old and new fields with new fields overwriting old ones.
+	$message_arr = array_merge($post, $message_arr);
+	if ( $clear_date ) {
+		$message_arr['post_date'] = current_time('mysql');
+		$message_arr['post_date_gmt'] = '';
+	}
+
+	return me_insert_message( $message_arr, $wp_error );
 }
 
 function me_get_message_status_list() {
@@ -167,15 +312,17 @@ function me_get_message() {
 
 }
 
-function me_get_message_field( $field, $message = null, $context = 'display' ) {
-    $message = me_get_message( $message );
- 
-    if ( !$message )
+function me_get_message_field($field, $message = null, $context = 'display') {
+    $message = me_get_message($message);
+
+    if (!$message) {
         return '';
- 
-    if ( !isset($message->$field) )
+    }
+
+    if (!isset($message->$field)) {
         return '';
- 
+    }
+
     return sanitize_post_field($field, $message->$field, $message->ID, $context);
 }
 
