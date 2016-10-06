6ef159de323296ce75eab28bfa184cb81389c00f
dakachi <david87dang@gmail.com>
2016-10-06 15:44:47 +0700

    update single listing control action

--- templates/single-listing/control-action.php
+++ templates/single-listing/control-action.php
@@ -1,10 +1,10 @@
-<?php
-$price = $listing->get_price();
-$pricing_unit = $listing->get_pricing_unit();
-?>
-
 <div class="me-action">
 	<?php if( 'purchasion' === $listing_type ) : ?>
+	<?php
+		$price = $listing->get_price();
+		$pricing_unit = $listing->get_pricing_unit();
+	?>
+
 		<span class="me-price">
 		<b itemprop="priceCurrency" content="USD">$</b>
 		<?php printf(__('<b itemprop="price" content="10">%d</b>%s', 'enginethemes'), $price, $pricing_unit) ?>
