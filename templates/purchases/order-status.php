661a16aea3a5ed9af73ab3bc6c6eb96f6b69a3e0
Ky Nguyen <kynh@youngworld.vn>
2016-10-07 16:29:53 +0700

    update transaction status, transaction process

--- templates/purchases/order-status.php
+++ templates/purchases/order-status.php
@@ -2,24 +2,29 @@
 	<div class="me-orderstatus-info">
 		<h5><?php echo __('Order status:', 'enginethemes'); ?></h5>
 		<div class="me-orderstatus">
-			<!-- <span class="me-order-complete">Complete</span>
-			<span class="me-order-pending">Pending</span>
-			<span class="me-order-close">Close</span>
-			<span class="me-order-resolve">Resolve</span> -->
-			<span class="me-order-dispute">Dispute</span>
+			<?php
+				me_print_order_status( $order_status );
+				if( 'me-disputed' === $order_status ) :
+			?>
 			<p class=""><i class="icon-me-status-info"></i>This order has been disputed by buyer. Resolve it as soon as possible.</p>
+			<?php
+				endif;
+			?>
 		</div>
+		<?php
+			$process_index = me_get_order_status_info( $order_status );
+		?>
 		<div class="me-line-process-order">
-			<div class="me-line-step-order active">
+			<div class="me-line-step-order <?php echo $process_index >= 1 ? 'active' : '' ?>">
 				<span>Check payment</span>
 			</div>
-			<div class="me-line-step-order active">
+			<div class="me-line-step-order <?php echo $process_index >= 2 ? 'active' : '' ?>">
 				<span>Active order</span>
 			</div>
-			<div class="me-line-step-order current">
+			<div class="me-line-step-order <?php echo $process_index >= 3 ? 'active' : '' ?>">
 				<span>Mark completed</span>
 			</div>
-			<div class="me-line-step-order">
+			<div class="me-line-step-order <?php echo $process_index >= 4 ? 'active' : '' ?>">
 				<span>Closed order</span>
 			</div>
 		</div>
