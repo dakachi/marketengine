53336a1334d483ae3a6c4c0de67e498cd72de4f0
dakachi <david87dang@gmail.com>
2016-10-11 13:57:59 +0700

    validate inquiry list view

--- includes/shortcodes/class-me-shortcodes-listing.php
+++ includes/shortcodes/class-me-shortcodes-listing.php
@@ -9,21 +9,21 @@ class ME_Shortcodes_Listing {
         add_shortcode('me_inquiry_form', array(__CLASS__, 'inquiry_form'));
     }
     public static function post_listing_form() {
-    	ob_start();
+        ob_start();
         me_get_template('post-listing/post-listing');
         $content = ob_get_clean();
         return $content;
     }
 
     public static function the_listing() {
-    	ob_start();
+        ob_start();
         me_get_template('taxonomy-listing_cat');
         $content = ob_get_clean();
         return $content;
     }
 
     public static function checkout_form() {
-        if( is_user_logged_in() ) {
+        if (is_user_logged_in()) {
             ob_start();
             me_get_template('checkout/checkout');
             $content = ob_get_clean();
@@ -37,8 +37,8 @@ class ME_Shortcodes_Listing {
         $paypal = ME_PPAdaptive_Request::instance();
         $paypal->complete_payment($_REQUEST);
 
-        $order_id = get_query_var( 'order-id' );
-        if($order_id) {
+        $order_id = get_query_var('order-id');
+        if ($order_id) {
             $order = new ME_Order($order_id);
             ob_start();
             me_get_template('checkout/confirm', array('order' => $order));
@@ -47,16 +47,26 @@ class ME_Shortcodes_Listing {
         }
     }
 
-
     public static function inquiry_form() {
-        if(!empty($_GET['id'])) {
+        $user_id = get_current_user_id();
+
+        if(!$user_id){
+            return __("You have to login to inquire listing.", "enginethemes");
+        }
+
+        if (!empty($_GET['id'])) {
             $listing_id = $_GET['id'];
-            $listing  = get_post($listing_id);
-            if($listing) {
+            $listing    = get_post($listing_id);
+            
+            if ($listing) {
                 $listing = new ME_Listing_Contact($listing);
+            }else {
+                return __("Invalid listing.", "enginethemes");
             }
 
-            //TODO: check current user can access inquiry list or note
+            if ($user_id == $listing->post_author) {
+                return __("You can not inquire your self.", "enginethemes");
+            }
 
             ob_start();
             me_get_template('inquiry/inquiry', array('listing' => $listing));
@@ -64,10 +74,13 @@ class ME_Shortcodes_Listing {
             return $content;
         }
 
-        if(!empty($_GET['inquiry_id'])) {
+        if (!empty($_GET['inquiry_id'])) {
             $inquiry_id = $_GET['inquiry_id'];
-            $inquiry  = me_get_message($inquiry_id);
-            //TODO: check current user can access inquiry list or note
+            $inquiry    = me_get_message($inquiry_id);            
+            
+            if($user_id != $inquiry->sender && $user_id != $inquiry->receiver) {
+                return __("You do not have permision to view this inquiry details.", "enginethemes");
+            }
             ob_start();
             me_get_template('inquiry/inquiry-message', array('inquiry' => $inquiry));
             $content = ob_get_clean();
@@ -77,11 +90,11 @@ class ME_Shortcodes_Listing {
     }
 
     public static function transaction_detail() {
-        if( is_user_logged_in() ) {
-            $transaction_id = get_query_var( 'transaction-id' );
-            $transaction = new ME_Order($transaction_id);
+        if (is_user_logged_in()) {
+            $transaction_id = get_query_var('transaction-id');
+            $transaction    = new ME_Order($transaction_id);
             ob_start();
-            me_get_template('purchases/transaction', array( 'transaction' => $transaction ) );
+            me_get_template('purchases/transaction', array('transaction' => $transaction));
             $content = ob_get_clean();
             return $content;
         } else {
