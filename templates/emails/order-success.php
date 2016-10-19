dd4ab8cd3cab53b057a54086282c9a2524fcf9af
dakachi <david87dang@gmail.com>
2016-10-19 10:31:42 +0700

    add email send to seller after payment success

--- templates/emails/order-success.php
+++ templates/emails/order-success.php
@@ -8,16 +8,17 @@ if (!defined('ABSPATH')) {
  * Email content send to seller when buyer order a listing successfull
  */
 ?>
-<p>Dear [display_name],</p>
-<p>Your listing - [link] - posted in [blogname] has a new order.</p>
-<p>Here are the order’s details:</p>
+<p><?php printf(__("Dear %s", "enginethemes"), $display_name); ?>,</p>
+<p><?php printf(__("Your listing - %s - posted in %s has a new order.", "enginethemes"), $listing_link, get_bloginfo('blogname') ); ?></p>
+<p><?php _e("Here are the order’s details:", "enginethemes"); ?></p>
 <ol>
-<li>Buyer Name: [buyer_name]</li>
-<li>Listing: [mjob_price]</li>
-<li>Total: [order_total]</li>
+	<li><?php printf(__("Buyer Name: %s", "enginethemes"), $buyer_name) ?></li>
+	<li><?php printf(__("Listing: %s", "enginethemes"), $listing_price) ?></li>
+	<li><?php printf(__("Total: %s", "enginethemes"), $total) ?></li>
 </ol>
 <p>
-For this order, the platform has deducted an amount of [commission] from your total earning of [order_price]. 
-You can view your order details here: [order_link].
+<?php printf(__("For this order, the platform has deducted an amount of %s from your total earning of %s. 
+You can view your order details here: %s.", "enginethemes"), $commission, $total, $order_link) ?>
 </p>
-<p>Sincerely,<br>[blogname]</p>
+<p><?php _e("Sincerely", "enginethemes"); ?>,
+<br><?php get_bloginfo('blogname'); ?></p>
