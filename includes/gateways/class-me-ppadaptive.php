d97256f5a483994ddbc9a0b8a2bd116670d9a063
dakachi <david87dang@gmail.com>
2016-10-26 08:48:23 +0700

    change comission fee to percent

--- includes/gateways/class-me-ppadaptive.php
+++ includes/gateways/class-me-ppadaptive.php
@@ -460,41 +460,41 @@ class ME_PPAdaptive_Request {
 
             $amount = me_get_order_item_meta($order_item_id, '_amount', true);
             if ($commission_fee > 0) {
-                $amount        = $amount - $commission_fee;
+                // $amount        = $amount - $commission_fee;
+                $commission    = ((float) $amount * (float) $commission_fee) / 100;
+                $amount        = $amount - $commission;
                 $receiver_list = array(
                     'receiverList.receiver(0).amount' => $amount,
                     'receiverList.receiver(0).email'  => me_get_order_item_meta($order_item_id, '_receive_email', true),
                     // 'receiverList.receiver(0).primary' => !$this->is_pay_primary(),
 
                     // admin receiver
-                    'receiverList.receiver(1).amount' => $this->get_commission_fee(),
+                    'receiverList.receiver(1).amount' => $commission,
                     'receiverList.receiver(1).email'  => $this->get_commission_email(),
                     //'receiverList.receiver(1).primary' => $this->is_pay_primary(),
                 );
+
+                // update order commission item
+                $commission_items = me_get_order_items($order->id, 'commission_item');
+                $receiver_1       = (object) array(
+                    'user_name'  => 'admin',
+                    'email'      => $this->get_commission_email(),
+                    'amount'     => $commission,
+                    'is_primary' => false,
+                );
+                if (!empty($commission_items)) {
+                    $order_item_id = $commission_items[0]->order_item_id;
+                    $order->update_commission($order_item_id, $receiver_1);
+                } else {
+                    $order->add_commission($receiver_1);
+                }
+                
             } else {
                 $receiver_list = array(
                     'receiverList.receiver(0).amount' => $amount,
                     'receiverList.receiver(0).email'  => me_get_order_item_meta($order_item_id, '_receive_email', true),
                     // 'receiverList.receiver(0).primary' => !$this->is_pay_primary(),
                 );
-            }
-
-        }
-
-        // add commission fee to order details
-        if ($commission_fee > 0) {
-            $commission_items = me_get_order_items($order->id, 'commission_item');
-            $receiver_1       = (object) array(
-                'user_name'  => 'admin',
-                'email'      => $this->get_commission_email(),
-                'amount'     => $this->get_commission_fee(),
-                'is_primary' => false,
-            );
-            if (!empty($commission_items)) {
-                $order_item_id = $commission_items[0]->order_item_id;
-                $order->update_commission($order_item_id, $receiver_1);
-            } else {
-                $order->add_commission($receiver_1);
             }
 
         }
@@ -512,7 +512,7 @@ class ME_PPAdaptive_Request {
      */
     public function setup_payment($order) {
         $currency = $order->get_currency_code();
-        if(!$currency) {
+        if (!$currency) {
             update_post_meta($order->id, '_me_currency_code', me_option('payment-currency-code', 'USD'));
             $currency = me_option('payment-currency-code', 'USD');
         }
@@ -611,8 +611,8 @@ class ME_PPAdaptive_Request {
                 me_add_order_item_meta($receiver->order_item_id, '_transaction_status', $transaction_info->transactionStatus);
             }
 
-            if(!empty($transaction_info->pendingReason)) {
-                $pending_reason = $transaction_info->pendingReason;
+            if (!empty($transaction_info->pendingReason)) {
+                $pending_reason  = $transaction_info->pendingReason;
                 $pending_message = $this->gateway->get_pending_message($pending_reason);
                 me_add_order_item_meta($receiver->order_item_id, '_pending_reason', $pending_message);
             }
