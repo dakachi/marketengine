3eb060920c26d9970b33fd8c2206c117ec4f4a91
dakachi <david87dang@gmail.com>
2016-10-26 09:41:19 +0700

    fix bug click on radio button not filter listing type

--- includes/widgets/class-me-widget-listing-types.php
+++ includes/widgets/class-me-widget-listing-types.php
@@ -55,7 +55,7 @@ class ME_Widget_Listing_Types extends WP_Widget {
             </div>
             <div class="me-listingtype-filter">
                 <label>
-                    <input type="radio" name="type" value="" <?php checked( '', $current); ?>>
+                    <input type="radio" name="type" value="" <?php checked( '', $current); ?> onclick="window.location.href='<?php echo remove_query_arg('type'); ?>'">
                     <a href="<?php echo remove_query_arg('type'); ?>" ><?php _e("All", "enginethemes"); ?></a>
                 </label>
             </div>
@@ -65,7 +65,7 @@ class ME_Widget_Listing_Types extends WP_Widget {
             ?>
             <div class="me-listingtype-filter">
                 <label>
-                    <input type="radio" name="type" value="<?php echo $key; ?>" <?php checked( $key, $current); ?>>
+                    <input type="radio" name="type" value="<?php echo $key; ?>" <?php checked( $key, $current); ?> onclick="window.location.href='<?php echo $link; ?>'">
                     <a href="<?php echo $link; ?>"><?php echo $type; ?></a>
                 </label>
             </div>
