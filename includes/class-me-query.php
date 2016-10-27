bff35cf5e9f8604e8a2b1f7db62be6addce5c792
dakachi <david87dang@gmail.com>
2016-10-27 11:57:45 +0700

    update validate billing info

--- includes/class-me-query.php
+++ includes/class-me-query.php
@@ -244,9 +244,9 @@ function me_filter_listing_query($query) {
         return $query;
     }
 
+    $query = me_sort_listing_query($query);
     $query = me_filter_price_query($query);
     $query = me_filter_listing_type_query($query);
-    $query = me_sort_listing_query($query);
 
     return $query;
 }
@@ -311,6 +311,9 @@ function me_sort_listing_query($query) {
             $query->set('meta_key', 'listing_price');
             $meta_query = array(
                 'relation' => 'AND',
+                'filter_price' => array(
+                    'key' => 'listing_price',
+                ),
                 'type' => array(
                     'key'     => '_me_listing_type',
                     'value'   => 'purchasion',
@@ -323,16 +326,18 @@ function me_sort_listing_query($query) {
             break;
         case 'price-desc':
             $query->set('meta_key', 'listing_price');
-            $query->set('meta_query', array(
-                array(
+            $meta_query = array(
+                'relation' => 'AND',
+                'filter_price' => array(
                     'key' => 'listing_price',
                 ),
-                array(
+                'type' => array(
                     'key'     => '_me_listing_type',
                     'value'   => 'purchasion',
                     'compare' => '=',
                 ),
-            ));
+            );
+            $query->set('meta_query', $meta_query);
             $query->set('orderby', 'meta_value_num');
             $query->set('order', 'desc');
             break;
