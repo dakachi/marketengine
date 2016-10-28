a92919a9051e25997e59f362f00eadc37e5bb78e
Ky Nguyen <kynh@youngworld.vn>
2016-10-28 13:50:14 +0700

    update content none listing

--- templates/loop/content-listing-none.php
+++ templates/loop/content-listing-none.php
@@ -1,1 +1,4 @@
-No listings to display
+<div class="">
+	<p><?php _e("Currently you don't have any listing to display","enginethemes"); ?></p>
+	<p><?php printf(__("You may start posting listing <a href='%s'>here</a>","enginethemes"), me_get_page_permalink('post_listing') ); ?></p>
+</div>
