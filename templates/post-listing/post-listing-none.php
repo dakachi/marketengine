16e0d07135a180dbab127f93ffdecdaa425305cd
Ky Nguyen <kynh@youngworld.vn>
2016-10-29 10:49:39 +0700

    update url on content listing none page

--- templates/post-listing/post-listing-none.php
+++ templates/post-listing/post-listing-none.php
@@ -1,5 +1,5 @@
 <div class="me-post-listing-none">
 	<h3><?php _e("Sorry Buddy!", "enginethemes"); ?></h3>
 	<p><?php _e("It seems that you don,t have permission to post listing.", "enginethemes"); ?></p>
-	<p><?php printf(__("You may <a href='%s'>register</a> or","enginethemes"), me_get_page_permalink('register_account') ); printf(__(" <a href='%s'>login</a> before post a listing","enginethemes"), me_get_page_permalink('user_account') ); ?> </p>
+	<p><?php printf(__("You may <a href='%s'>register</a> or","enginethemes"), me_get_auth_url('register') ); printf(__(" <a href='%s'>login</a> before post a listing","enginethemes"), me_get_page_permalink('user_account') ); ?> </p>
 </div>
