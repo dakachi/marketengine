3fc84aa4c1449bad589bf50afd6d45347d346e2a
thoitranyw <thoitq@youngworld.vn>
2016-10-31 18:03:31 +0700

    update style checkout page

--- templates/checkout/checkout.php
+++ templates/checkout/checkout.php
@@ -28,10 +28,12 @@ $cart_items = me_get_cart_items();
 				?>
 			</div>
 			<div class="me-col-md-3">
-				<?php 
-				// seller information
-				me_get_template('user-info', array('author_id' => $listing->post_author));
-				?>
+				<div class="me-checkout-sidebar">
+					<?php 
+					// seller information
+					me_get_template('user-info', array('author_id' => $listing->post_author));
+					?>
+				</div>
 			</div>
 		</div>
 		
