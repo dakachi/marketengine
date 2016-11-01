78b9e5430af635997cf7d917bbda452103fb410a
Ky Nguyen <kynh@youngworld.vn>
2016-11-01 14:52:07 +0700

    update template user profile, edit profile

--- templates/post-listing/listing-tags.php
+++ templates/post-listing/listing-tags.php
@@ -7,11 +7,11 @@
 	<div class="marketengine-input-field">
 	    <?php
 	    	$listing_tag = !empty($_POST['listing_tag']) ? $_POST['listing_tag'] : $default;
-	    	me_post_tags_meta_box($listing_tag, 'listing_tag'); 
+	    	me_post_tags_meta_box($listing_tag, 'listing_tag');
 	    ?>
 	</div>
 
 	<?php do_action('marketengine_post_listing_tags_form_end'); ?>
-	
+
 </div>
 <?php do_action('marketengine_after_post_listing_tags_form'); ?>
