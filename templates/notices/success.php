51d6ec54f2ee389cf466f6fe65276b29e4120cb8
thoitranyw <thoitq@youngworld.vn>
2016-11-02 17:26:41 +0700

    fix html nitifi update profile success

--- templates/notices/success.php
+++ templates/notices/success.php
@@ -2,7 +2,7 @@
 $me_notices = me_get_notices('success');
 foreach ($me_notices as $key => $notice):
     ?>
-		<div class="alert alert-success" role="alert">
+		<div class="me-authen-success" role="alert">
 			<?php echo $notice ?>
 		</div>
 	<?php
