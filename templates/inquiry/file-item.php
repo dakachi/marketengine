9ccbc6128e8d7c45f54fe10d5f92ca60c8e11d94
dakachi <david87dang@gmail.com>
2016-11-02 10:29:56 +0700

    update file item

--- templates/inquiry/file-item.php
+++ templates/inquiry/file-item.php
@@ -8,7 +8,15 @@ switch ($file_type['ext']) {
 	case 'jpeg':
 	case 'png':
 	case 'jpeg':
+	case 'jpg' :
+	case 'gif' :
 		echo '<i class="fa fa-picture-pdf-o" aria-hidden="true"></i>';
+		?>
+		<a href="<?php echo $url; ?>" class="mess-file-item" title="<?php echo $name; ?>">
+			<img src="<?php echo $url; ?>">
+		</a>
+		<?php
+		return ;
 		break;
 	case 'pdf' :
 		echo '<i class="fa fa-file-pdf-o" aria-hidden="true"></i>';
