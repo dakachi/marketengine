c86d985a67fb008edc91e7e4694918a090a01f94
dakachi <david87dang@gmail.com>
2016-11-03 13:43:00 +0700

    update class me listing

--- includes/listings/class-me-listing.php
+++ includes/listings/class-me-listing.php
@@ -393,8 +393,7 @@ class ME_Listing {
      * @return bool
      */
     public function is_available() {
-        // TODO: cap nhat cho nay
-        return 'listing' === $this->post_type;
+        return  apply_filters('marketengine_lisitng_is_available', 'listing' === $this->post_type, $this->ID);
     }
 
     public function get_edit_url(){
