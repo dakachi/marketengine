162e36d709eb5d76742c2f18dfa3bf8f971a5761
Ky Nguyen <kynh@youngworld.vn>
2016-11-04 17:45:41 +0700

    test email confirm

--- templates/emails/activation.php
+++ templates/emails/activation.php
@@ -1,5 +1,5 @@
 <p>Hello [display_name],</p>
 <p>You have successfully registered an account with [blogname]. Here is your account information:</p>
 <ol><li>Username: [user_login]</li><li>Email: [user_email]</li></ol>
-<p><?php _e('Please click the link below to confirm your email address. <br/> %s', $activate_email_link); ?></p>
+<p><?php printf(_e('Please click the link below to confirm your email address. <br/> %s', $activate_email_link)); ?></p>
 <p>Thank you and welcome to [blogname].</p>
