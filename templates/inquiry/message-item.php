ec30b3ecd8cec15a20f81e778ede0db30ddd83e3
dakachi <david87dang@gmail.com>
2016-11-04 14:34:34 +0700

    update message item

--- templates/inquiry/message-item.php
+++ templates/inquiry/message-item.php
@@ -1,9 +1,13 @@
 <li>
-	<span class="me-mgs-author-avatar">
-		<?php echo me_get_avatar( $message->sender, '36'); ?>
-	</span>
+	<a href="<?php echo get_author_posts_url($message->sender); ?>" >
+		<span class="me-mgs-author-avatar">
+			<?php echo me_get_avatar( $message->sender, '36'); ?>
+		</span>
+	</a>
 	<div class="me-message-author">
-		<h4 class="me-author"><?php echo get_the_author_meta( 'display_name', $message->sender ); ?></h4>
+		<a href="<?php echo get_author_posts_url($message->sender); ?>" >
+			<h4 class="me-author"><?php echo get_the_author_meta( 'display_name', $message->sender ); ?></h4>
+		</a>
 		<p><?php echo apply_filters( 'the_marketengine_message', $message->post_content ); ?></p>
 		<span><?php echo human_time_diff( strtotime($message->post_date) ); ?></span>
 	</div>
