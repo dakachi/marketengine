e5117454b83e4fd20d6e8bf892f682ed5ad69ba5
thoitranyw <thoitq@youngworld.vn>
2016-11-05 12:22:19 +0700

    style listing detail

--- templates/content-single-listing.php
+++ templates/content-single-listing.php
@@ -17,67 +17,71 @@ $listing_type = $listing->get_listing_type();
 $is_owner = $listing->post_author == get_current_user_id();
 $listing_status = get_post_status_object($listing->post_status);
 ?>
-<div class="marketengine me-container">
-	<div itemscope itemtype="http://schema.org/Product" class="marketengine-listing-detail">
-		<div class="me-row">
-			<div class="me-col-md-12">
-				<?php me_get_template('single-listing/title');?>
-				<?php me_get_template('single-listing/statistic', array('listing' => $listing));?>
+<div id="marketengine-page">
+	<div class="marketengine me-container">
+		<div itemscope itemtype="http://schema.org/Product" class="marketengine-listing-detail">
+			<div class="me-row">
+				<div class="me-col-md-12">
+					<?php me_get_template('single-listing/title');?>
+					<?php me_get_template('single-listing/statistic', array('listing' => $listing));?>
+				</div>
 			</div>
-		</div>
-		<div class="me-row">
-			<div class="me-col-md-9">
+			<div class="me-row">
+				<div class="me-col-md-9">
 
-				<?php do_action('marketengine_before_single_listing_details'); ?>
+					<?php do_action('marketengine_before_single_listing_details'); ?>
 
-				<div class="marketengine-content-detail">
-					<?php me_get_template('single-listing/gallery', array('listing' => $listing));?>
-					<?php me_get_template('single-listing/description', array('listing' => $listing));?>
-					<?php me_get_template('single-listing/rating', array('listing' => $listing));?>
+					<div class="marketengine-content-detail">
+						<?php me_get_template('single-listing/gallery', array('listing' => $listing));?>
+						<div class="me-box-shadow">
+							<?php me_get_template('single-listing/description', array('listing' => $listing));?>
+							<?php me_get_template('single-listing/rating', array('listing' => $listing));?>
+						</div>
+					</div>
+
+					<?php do_action('marketengine_after_single_listing_details'); ?>
+
 				</div>
 
-				<?php do_action('marketengine_after_single_listing_details'); ?>
+				<?php if(!me_is_activated_user()) : ?>
+				<div class="me-col-md-3">
+					<p><?php _e("Sorry! Only active account can buy listings. Please check mail box to activate your account.", "enginethemes"); ?></p>
+				<?php
+					$profile_link = me_get_page_permalink('user_account');
+	                $activate_email_link = add_query_arg(array( 'resend-confirmation-email' => true, '_wpnonce' => wp_create_nonce('me-resend_confirmation_email') ), $profile_link);
+	            ?>
+	                <p><?php _e('<a href="'.$activate_email_link.'">' . sprintf( __("Resend activation email.", "enginethemes") ) . '</a>'); ?></p>
+				</div>
+				<?php endif; ?>
 
-			</div>
+				<div class="me-col-md-3">
 
-			<?php if(!me_is_activated_user()) : ?>
-			<div class="me-col-md-3">
-				<p><?php _e("Sorry! Only active account can buy listings. Please check mail box to activate your account.", "enginethemes"); ?></p>
-			<?php
-				$profile_link = me_get_page_permalink('user_account');
-                $activate_email_link = add_query_arg(array( 'resend-confirmation-email' => true, '_wpnonce' => wp_create_nonce('me-resend_confirmation_email') ), $profile_link);
-            ?>
-                <p><?php _e('<a href="'.$activate_email_link.'">' . sprintf( __("Resend activation email.", "enginethemes") ) . '</a>'); ?></p>
-			</div>
-			<?php endif; ?>
+					<?php do_action('marketengine_before_single_listing_sidebar'); ?>
 
-			<div class="me-col-md-3">
+					<div class="marketengine-sidebar-detail">
 
-				<?php do_action('marketengine_before_single_listing_sidebar'); ?>
-
-				<div class="marketengine-sidebar-detail">
-
-					<?php
-					if($listing_type) :
-						if( $is_owner ) :
-							me_get_template('single-listing/status', array('listing_status' => $listing_status) );
-							me_get_template('single-listing/control-action', array('listing_type' => $listing_type , 'listing' => $listing, 'listing_status' => $listing_status) );
-						else :
-							me_get_template('single-listing/'. $listing_type , array('listing' => $listing));
+						<?php
+						if($listing_type) :
+							if( $is_owner ) :
+								me_get_template('single-listing/status', array('listing_status' => $listing_status) );
+								me_get_template('single-listing/control-action', array('listing_type' => $listing_type , 'listing' => $listing, 'listing_status' => $listing_status) );
+							else :
+								me_get_template('single-listing/'. $listing_type , array('listing' => $listing));
+							endif;
 						endif;
-					endif;
-					?>
-					<?php me_get_template('single-listing/category');?>
-					<?php
-					if( !$is_owner ) :
-						me_get_template('user-info', array('author_id' => $listing->post_author));
-						me_get_template('single-listing/report');
-					endif;
-					?>
+						?>
+						<?php me_get_template('single-listing/category');?>
+						<?php
+						if( !$is_owner ) :
+							me_get_template('user-info', array('author_id' => $listing->post_author));
+							me_get_template('single-listing/report');
+						endif;
+						?>
+					</div>
+
+					<?php do_action('marketengine_after_single_listing_sidebar'); ?>
+
 				</div>
-
-				<?php do_action('marketengine_after_single_listing_sidebar'); ?>
-
 			</div>
 		</div>
 	</div>
--- templates/single-listing/status.php
+++ templates/single-listing/status.php
@@ -1,5 +1,5 @@
 <div class="me-status">
-	<div class="marketengine-ribbon-<?php echo str_replace('me-', '', $listing_status->name); ?>">
-		<span class="me-ribbon-content"><?php echo ucfirst($listing_status->label); ?></span>
+	<div class="me-label-<?php echo str_replace('me-', '', $listing_status->name); ?>">
+		<span><?php echo ucfirst($listing_status->label); ?></span>
 	</div>
 </div>
