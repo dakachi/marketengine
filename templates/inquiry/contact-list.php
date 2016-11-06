103daa51864785b6e78312b33f4e6d5a5cf2f8d1
dakachi <david87dang@gmail.com>
2016-11-06 23:24:16 +0700

    add test contact list

--- templates/inquiry/contact-list.php
+++ templates/inquiry/contact-list.php
@@ -10,8 +10,8 @@ $messages = new ME_Message_Query(array('post_parent' => $listing->ID, 'post_type
 		<input type="text" placeholder="<?php echo __('Search buyer'); ?>">
 		<span class="me-user-search-btn"><i class="icon-me-search"></i></span>
 	</div> -->
-	<div class="me-contact-user-wrap" style="max-height: 620px;overflow: hidden;overflow-y: scroll;" >
-		<ul id="contact-list" class="me-contact-user-list" data-id="<?php the_ID(); ?>" >
+	<div class="me-contact-user-wrap" id="contact-list" data-id="<?php the_ID(); ?>" style="max-height: 620px;overflow: hidden;overflow-y: scroll;" >
+		<ul  class="me-contact-user-list"  >
 			<?php while($messages->have_posts()): $messages->the_post(); ?>
 				<?php me_get_template('inquiry/contact-item'); ?>
 			<?php endwhile; ?>
