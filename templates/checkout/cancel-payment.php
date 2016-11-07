ed5f5711f642505f6379d1854a08ac3a8284f135
kn-youngworld <kynh@youngworld.vn>
2016-11-07 14:39:51 +0700

    update cancel payment page

--- templates/checkout/cancel-payment.php
+++ templates/checkout/cancel-payment.php
@@ -2,5 +2,5 @@
 	<h3><?php _e("Cancelling Payment", "enginethemes"); ?></h3>
 	<p class="me-cancel-payment-1"><?php _e("There was something went wrong with your payment!", "enginethemes"); ?></p>
 	<p class="me-cancel-payment-2"><?php _e("Your payment can be completed in the order detail", "enginethemes"); ?></p>
-	<a href="" class="marketengine-btn"><?php printf(__('TO ORDER DETAIL', 'enginethemes'), $order->get_order_detail_url()); ?></a>
+	<p><a href="<?php echo $order->get_order_detail_url() ?>"><?php _e('TO ORDER DETAIL', 'enginethemes'); ?></a></p>
 </div>
