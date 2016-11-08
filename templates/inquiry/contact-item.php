61bea2da6f27ad87bc4fbfde39227dbfb532156d
dakachi <david87dang@gmail.com>
2016-11-08 10:22:25 +0700

    add show  message count

--- templates/inquiry/contact-item.php
+++ templates/inquiry/contact-item.php
@@ -1,4 +1,7 @@
-<?php $message = me_get_message(); ?>
+<?php 
+$message = me_get_message(); 
+$new_message = me_get_message_meta($message->ID, '_me_recevier_new_message', true);
+?>
 
 <li <?php if($message->ID == $_GET['inquiry_id']) {echo 'class="active"';} ?>>
 	<a href="<?php echo add_query_arg('inquiry_id', $message->ID); ?>">
@@ -7,6 +10,11 @@
 		</span>
 		<span class="me-contact-author">
 			<span><?php echo get_the_author_meta( 'display_name', $message->sender ); ?></span>
+
+			<?php if($new_message) : ?>
+				<span class="me-message-count"><?php echo $new_message; ?>+</span>
+			<?php endif; ?>
+
 		</span>
 	</a>
 </li>
