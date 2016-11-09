8ef4010fc8c65d4d5cc718d6af074e3c0f1a04e0
dakachi <david87dang@gmail.com>
2016-11-09 09:20:50 +0700

    update text language

--- templates/admin/filter.php
+++ templates/admin/filter.php
@@ -1,32 +1,32 @@
 <div class="me-report-filter">
 
-		<span class="me-pick-date-box">
-			<form action="" method="get">
-				<input name="page" value="marketengine" type="hidden" />
-				<input name="tab" value="<?php echo empty($_REQUEST['tab']) ? 'listing' : $_REQUEST['tab']; ?>" type="hidden" />
-				<select name="quant" >
-					<option value="day"><?php _e("Day", "enginethemes"); ?></option>
-					<option value="week"><?php _e("Week", "enginethemes"); ?></option>
-					<option value="month"><?php _e("Month", "enginethemes"); ?></option>
-					<option value="quarter"><?php _e("Quarter", "enginethemes"); ?></option>
-					<option value="year"><?php _e("Year", "enginethemes"); ?></option>
-				</select>
+	<span class="me-pick-date-box">
+		<form action="" method="get">
+			<input name="page" value="marketengine" type="hidden" />
+			<input name="tab" value="<?php echo empty($_REQUEST['tab']) ? 'listing' : $_REQUEST['tab']; ?>" type="hidden" />
+			<select name="quant" >
+				<option value="day"><?php _e("Day", "enginethemes"); ?></option>
+				<option value="week"><?php _e("Week", "enginethemes"); ?></option>
+				<option value="month"><?php _e("Month", "enginethemes"); ?></option>
+				<option value="quarter"><?php _e("Quarter", "enginethemes"); ?></option>
+				<option value="year"><?php _e("Year", "enginethemes"); ?></option>
+			</select>
 
-				<span class="me-report-start-date"><?php _e("From", "enginethemes"); ?></span>
-				<span class="me-pick-date">
-					<input id="me-pick-date-1" type="text" name="from_date" value="<?php echo empty($_REQUEST['from_date']) ? 'listing' : $_REQUEST['from_date']; ?>">
-				</span>
-				<span class="me-report-end-date"><?php _e("To", "enginethemes"); ?></span>
-				<span class="me-pick-date">
-					<input id="me-pick-date-2" type="text" name="to_date" value="<?php echo empty($_REQUEST['to_date']) ? 'listing' : $_REQUEST['to_date']; ?>">
-				</span>
+			<span class="me-report-start-date"><?php _e("From", "enginethemes"); ?></span>
+			<span class="me-pick-date">
+				<input id="me-pick-date-1" type="text" name="from_date" value="<?php echo empty($_REQUEST['from_date']) ? 'listing' : $_REQUEST['from_date']; ?>">
+			</span>
+			<span class="me-report-end-date"><?php _e("To", "enginethemes"); ?></span>
+			<span class="me-pick-date">
+				<input id="me-pick-date-2" type="text" name="to_date" value="<?php echo empty($_REQUEST['to_date']) ? 'listing' : $_REQUEST['to_date']; ?>">
+			</span>
 
-				<input type="submit" class="me-report-submit-btn" value="Filter">
+			<input type="submit" class="me-report-submit-btn" value="<?php _e("Filter", "enginethemes"); ?>">
 
-			</form>
-		</span>
-		<span class="me-export-report">
-			<a href="<?php echo add_query_arg('export', 'csv'); ?>"><?php _e("Export", "enginethemes"); ?></a>
-		</span>
+		</form>
+	</span>
+	<span class="me-export-report">
+		<a href="<?php echo add_query_arg('export', 'csv'); ?>"><?php _e("Export", "enginethemes"); ?></a>
+	</span>
 
 </div>
