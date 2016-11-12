b6fc1c722e4a4adad50f54ac04249dd35c7c396f
thoitranyw <thoitq@youngworld.vn>
2016-11-12 12:03:23 +0700

    fix menu category header

--- templates/global/shop-categories.php
+++ templates/global/shop-categories.php
@@ -8,22 +8,21 @@
  * @return string
  */
 $listing_categories = me_get_categories('listing_category');
-if( $listing_categories ) :
 ?>
-    <ul class="me-shopcategories">
+    <ul class="me-shopcategories <?php if( $listing_categories ) echo 'me-has-category'; ?>">
         <li>
             <span class="marketengine-bar-title"><i class="icon-me-list"></i><?php echo __('SHOP CATEGORIES', 'enginethemes'); ?></span>
-            <nav class="me-nav-category">
-                <div class="me-container">
-                    <span class="me-triangle-top"></span>
-                    <ul class="me-menu-category">
-                        <?php foreach( $listing_categories as $category ) : ?>
-                            <li><a href="<?php echo get_term_link($category->term_id) ?>"><?php echo esc_html($category->name); ?></a></li>
-                        <?php endforeach; ?>
-                    </ul>
-                </div>
-            </nav>
+            <?php if( $listing_categories ) : ?>
+                <nav class="me-nav-category">
+                    <div class="me-container">
+                        <span class="me-triangle-top"></span>
+                        <ul class="me-menu-category">
+                            <?php foreach( $listing_categories as $category ) : ?>
+                                <li><a href="<?php echo get_term_link($category->term_id) ?>"><?php echo esc_html($category->name); ?></a></li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </div>
+                </nav>
+            <?php endif; ?>
         </li>
     </ul>
-<?php
-endif;
