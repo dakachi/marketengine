e73d294c2fd2178f2742c91e3e3e5682a48c2cb5
dakachi <david87dang@gmail.com>
2016-11-15 10:48:06 +0700

    update class admin setup wizard

--- assets/admin/setup-wizard.js
+++ assets/admin/setup-wizard.js
@@ -1,37 +1,62 @@
 (function($) {
     $(document).ready(function() {
-        //=== Click continue button
-        $('.me-scontinue-btn').on('click', function(event) {
-            event.preventDefault();
+        $('.wizard-start').on('click', function(event) {
+            //event.preventDefault();
             var target = event.currentTarget;
             var parent_section = $(target).parents('.me-setup-section');
             var parent_container = $(target).parents('.me-setup-container');
-            var data_step = $(parent_container).data('step');
+            var data_step = parent_container.data('step');
             var data_next = data_step + 1;
             // activity
-            $(parent_section).addClass('me-setup-section-loading');
-            setTimeout(function() {
-                $(parent_section).removeClass('me-setup-section-loading');
-                $(parent_container).removeClass('active');
-                $('.me-setup-container').eq(data_next).addClass('active');
-                $('.me-setup-line-step').eq(data_next).addClass('active');
-            }, 100);
+            parent_container.removeClass('active');
+            $('.me-setup-container').eq(data_next).addClass('active');
+            $('.me-setup-line-step').eq(data_next).addClass('active');
         });
-        //=== Click previous button
-        $('.me-sprevious-btn').on('click', function(event) {
-            event.preventDefault();
+        //=== Click continue button
+        $('.me-next').on('click', function(event) {
+            //event.preventDefault();
+            var $target = $(event.currentTarget);
+            var $parent_section = $target.parents('.me-setup-section');
+            var $parent_container = $target.parents('.me-setup-container');
+            var data_step = $parent_container.data('step');
+            var data_next = data_step + 1;
+            // activity
+            $.ajax({
+                type: 'post',
+                url: me_globals.ajaxurl,
+                data: {
+                    action : 'me-do-setup',
+                    _wpnonce: $('#_wpnonce').val(),
+                    step: $parent_container.find('input[name="step"]').val()
+                },
+                beforeSend: function() {
+                    $parent_section.addClass('me-setup-section-loading');
+                },
+                success: function(res, xhr) {
+                    $parent_section.removeClass('me-setup-section-loading');
+                    $parent_container.removeClass('active');
+                    $('.me-setup-container').eq(data_next).addClass('active');
+                    $('.me-setup-line-step').eq(data_next).addClass('active');
+                }
+            });
+        });
+
+        //=== Click skip button
+        $('.me-skip-btn').on('click', function(event) {
+            //event.preventDefault();
             var target = event.currentTarget;
+            var parent_section = $(target).parents('.me-setup-section');
             var parent_container = $(target).parents('.me-setup-container');
-            var data_step = $(parent_container).data('step');
-            var data_prev = data_step - 1;
-            //activity
-            $(parent_container).removeClass('active');
-            $('.me-setup-container').eq(data_prev).addClass('active');
-            $('.me-setup-line-step').eq(data_step).removeClass('active');
+            var data_step = parent_container.data('step');
+            var data_next = data_step + 1;
+            // activity
+            parent_container.removeClass('active');
+            $('.me-setup-container').eq(data_next).addClass('active');
+            $('.me-setup-line-step').eq(data_next).addClass('active');
         });
         $('.me-setup-add-cat').on('click', function(event) {
             var parent_sfield = $(this).parent();
-            $(parent_sfield).append('<input type="text" /> <input type="text" /><small>More categories can be added later in MarketEngine settings</small>')
+            $(parent_sfield).find('.more-cat').slideDown();
             $(this).hide();
         });
     });
