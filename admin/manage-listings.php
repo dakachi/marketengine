cea250fd3d18312c4a2e4fdc622c83ae555fe814
dakachi <david87dang@gmail.com>
2016-11-18 13:15:55 +0700

    update mangage listing

--- admin/manage-listings.php
+++ admin/manage-listings.php
@@ -8,7 +8,8 @@ if (!defined('ABSPATH')) {
  * Manage listings in WP post screen
  */
 
-function post_row_actions($actions, $post) {
+function post_row_actions($actions, $post)
+{
     if ($post && 'listing' == $post->post_type) {
         return array();
     }
@@ -16,55 +17,71 @@ function post_row_actions($actions, $post) {
 }
 add_filter('post_row_actions', 'post_row_actions', 10, 2);
 
-function me_listing_columns($existing_columns) {
+function me_listing_columns($existing_columns)
+{
     if (empty($existing_columns) && !is_array($existing_columns)) {
         $existing_columns = array();
     }
 
-    unset($existing_columns['comments'], $existing_columns['date'], $existing_columns['author']);
+    unset($existing_columns['comments'], $existing_columns['date'], $existing_columns['title'], $existing_columns['author']);
 
     $columns = array();
 
+    $columns['post_title']       = 'Title';
     $columns['type']             = 'Type';
     $columns['listing_category'] = 'Categories';
     $columns['price']            = 'Price';
-    $columns['author']           = 'Author';
+    $columns['author_profile']   = 'Author';
     $columns['date']             = 'Posted';
 
     return array_merge($existing_columns, $columns);
 }
 add_filter('manage_listing_posts_columns', 'me_listing_columns');
 
-function me_render_listing_columns($column) {
+function me_render_listing_columns($column)
+{
     global $post, $wpdb;
 
     switch ($column) {
-    case 'type':
-        $listing_type = get_post_meta($post->ID, '_me_listing_type', true);
-        if (!empty($listing_type)) {
-         	echo esc_html(me_get_listing_type_lable($listing_type));
-        }
-        break;
-    
-    case 'listing_category':
-        $categoryarray = array();
-        $categories    = get_the_terms($post->ID, 'listing_category');
-        if ($categories) {
-            foreach ($categories as $category) {
-                $categoryarray[] = edit_term_link($category->name, '', '', $category, false);
+        case 'post_title':
+            echo '<a href="' . get_permalink($post->ID) . '" target="_blank" >' . esc_html(get_the_title($post->ID)) . '</a>';
+            break;
+
+        case 'author_profile':
+            printf(
+                '<a target="_blank" href="%1$s" title="%2$s" rel="author">%3$s</a>',
+                esc_url(get_author_posts_url($post->post_author, get_the_author_meta( 'display_name', '$post->post_author' ))),
+                esc_attr(sprintf(__('Posts by %s'), get_the_author())),
+                get_the_author()
+            );
+            break;
+
+        case 'type':
+            $listing_type = get_post_meta($post->ID, '_me_listing_type', true);
+            if (!empty($listing_type)) {
+                echo esc_html(me_get_listing_type_lable($listing_type));
             }
-            echo implode(', ', $categoryarray);
-        } else {
-            echo '&ndash;';
-        }
-        break;
-    
-    case 'price':
-        $price = get_post_meta($post->ID, 'listing_price', true);
-        if($price) {
-            echo me_price_html($price);    
-        }
-        break;
+            break;
+
+        case 'listing_category':
+            $categoryarray = array();
+            $categories    = get_the_terms($post->ID, 'listing_category');
+            if ($categories) {
+                foreach ($categories as $category) {
+                    $categoryarray[] = edit_term_link($category->name, '', '', $category, false);
+                }
+                echo implode(', ', $categoryarray);
+            } else {
+                echo '&ndash;';
+            }
+            break;
+
+        case 'price':
+            $price = get_post_meta($post->ID, 'listing_price', true);
+            if ($price) {
+                echo me_price_html($price);
+            }
+            break;
     }
 }
-add_action('manage_listing_posts_custom_column',  'me_render_listing_columns', 2);
+add_action('manage_listing_posts_custom_column', 'me_render_listing_columns', 2);
