36e016ea084ef5cdbe3055c21a536e8e48faeede
dakachi <david87dang@gmail.com>
2016-11-18 15:10:08 +0700

    update text domain

--- admin/class-me-setupwizard.php
+++ admin/class-me-setupwizard.php
@@ -270,15 +270,15 @@ class ME_Setup_Wizard
 					<h2><?php _e("Personalize", "enginethemes");?></h2>
 					<div class="me-sfield-group">
 						<label for="me-setup-listing"><?php _e("1- How should people understand “listings” in your marketplace?", "enginethemes");?></label>
-						<input id="me-setup-listing" type="text" name="listing_label">
+						<input id="me-setup-listing" type="text" name="listing_label" value="<?php echo me_option('listing-label', 'Listing') ?>">
 					</div>
 					<div class="me-sfield-group">
 						<label for="me-setup-seller"><?php _e("2- What should we call the Seller role in your marketplace?", "enginethemes");?></label>
-						<input id="me-setup-seller" type="text" name="seller_label">
+						<input id="me-setup-seller" type="text" name="seller_label" value="<?php echo me_option('seller-label', 'Seller') ?>">
 					</div>
 					<div class="me-sfield-group">
 						<label for="me-setup-buyer"><?php _e("3- What should we call the Buyer role in your marketplace?", "enginethemes");?></label>
-						<input id="me-setup-buyer" type="text" name="buyer_label">
+						<input id="me-setup-buyer" type="text" name="buyer_label" value="<?php echo me_option('buyer-label', 'Buyer') ?>">
 					</div>
 					<div class="me-setup-control">
 						<a href="#payment" class="me-sprevious-btn me-skip-btn"><?php _e("Skip this step", "enginethemes");?></a>
