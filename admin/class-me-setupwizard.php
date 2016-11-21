de89e8d2f81d4b6ee183ef9d5f2ee312deab9bdf
dakachi <david87dang@gmail.com>
2016-11-21 11:12:36 +0700

    add test to setup wizard

--- admin/class-me-setupwizard.php
+++ admin/class-me-setupwizard.php
@@ -114,6 +114,7 @@ class ME_Setup_Wizard
     		update_option( 'me-added-sample-data', 1 );
     		marketengine_add_sample_listing();
     	}
+    	echo 1111;
     }
 
     public function admin_menus()
