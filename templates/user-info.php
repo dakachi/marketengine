1d30cb9c0c75009c38ac7fc17fa439aa5de486bf
thoitranyw <thoitq@youngworld.vn>
2016-11-23 13:49:34 +0700

    fix seller profile

--- templates/user-info.php
+++ templates/user-info.php
@@ -12,7 +12,7 @@ $display_name = get_the_author_meta('display_name', $author_id);
 $location = get_the_author_meta('location', $author_id);
 $member_sinced = date_i18n( get_option( 'date_format' ), strtotime(get_the_author_meta( 'user_registered', $author_id )) );
 ?>
-<div class="me-authors <?php echo !empty($class) ? $class : ''; ?>">
+<div class="me-authors me-authors-xs <?php echo !empty($class) ? $class : ''; ?>">
 	<span class="me-avatar">
 		<?php echo me_get_avatar( $author_id ); ?>
 		<b><?php echo $display_name; ?></b>
