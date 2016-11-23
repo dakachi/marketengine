03795ac86467ab90d9e254b8fb457c8f5fb4c32a
dakachi <david87dang@gmail.com>
2016-11-23 11:43:11 +0700

    validate option number field

--- includes/handle-options/class-me-options-handle.php
+++ includes/handle-options/class-me-options-handle.php
@@ -4,11 +4,13 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-class ME_Options_Handle{
+class ME_Options_Handle
+{
 
     public $_instance;
 
-    public function get_instance() {
+    public function get_instance()
+    {
         if (is_null(self::$_instance)) {
             self::$_instance = new self();
         }
@@ -20,41 +22,44 @@ class ME_Options_Handle{
      *  @author     KyNguyen
      */
 
-    public function option_sync() {
-        if(!current_user_can( 'manage_options' )) {
-            wp_send_json( array('success' => false , 'msg' => __("You do not have permission to change option.", 'enginethemes')) );
+    public function option_sync()
+    {
+        if (!current_user_can('manage_options')) {
+            wp_send_json(array('success' => false, 'msg' => __("You do not have permission to change option.", 'enginethemes')));
         }
 
         $request = $_REQUEST;
 
-        $name = $request['name'];
+        $name  = $request['name'];
         $value = array();
 
         $options = ME_Options::get_instance();
 
-        if( is_string($request['value']) ){
+        if (is_string($request['value'])) {
             $request['value'] = stripslashes($request['value']);
         }
 
-        if( is_int($request['value']) ){
-            wp_send_json( 'adas' );
+        if (is_int($request['value'])) {
+            wp_send_json('adas');
         }
 
-        $value = $request['value'];
+        $value          = $request['value'];
+        if(!empty($request['type']) && $request['type'] == 'number') {
+            $value =(float) $value;
+        }
+
         $options->$name = $value;
         $options->save();
 
         $options_arr = $options->get_all_current_options();
-        $id = array_search($name, array_keys($options_arr));
-        $response = array(
+        $id          = array_search($name, array_keys($options_arr));
+        $response    = array(
             'success' => true,
-            'data' => array(
-                'ID'        => $id
-            ) ,
-            'msg' => __("Update option successfully!", 'enginethemes'),
+            'data'    => array(
+                'ID' => $id,
+            ),
+            'msg'     => __("Update option successfully!", 'enginethemes'),
         );
-        wp_send_json( $response );
+        wp_send_json($response);
     }
 }
-
-
