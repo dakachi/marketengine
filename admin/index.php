610bea6cee6cb6908412c045310bcb18b18a29f3
dakachi <david87dang@gmail.com>
2016-11-25 15:00:04 +0700

    remove admin option file

--- admin/index.php
+++ admin/index.php
@@ -12,7 +12,6 @@ if(!is_admin()) return ;
 require_once ME_PLUGIN_PATH . '/admin/fields/class-auto-loader.php';
 
 require_once ME_PLUGIN_PATH . '/admin/admin-panel.php';
-require_once ME_PLUGIN_PATH . '/admin/admin-options.php';
 
 require_once ME_PLUGIN_PATH . '/admin/reports.php';
 require_once ME_PLUGIN_PATH . '/admin/manage-listings.php';
