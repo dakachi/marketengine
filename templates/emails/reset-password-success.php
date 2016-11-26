2dc0bb517153fdeded1accb52bd53008bf3b6b28
Ky Nguyen <kynh@youngworld.vn>
2016-11-26 10:38:09 +0700

    update my listing action, reset password email template

--- templates/emails/reset-password-success.php
+++ templates/emails/reset-password-success.php
@@ -1,3 +1,20 @@
+<?php
+/**
+ * 	The Email template for sending to user when they reset password successfully.
+ * 	This template can be overridden by copying it to yourtheme/marketengine/emails/reset-password-success.php.
+ *
+ * @author 		EngineThemes
+ * @package 	MarketEngine/Templates
+ *
+ * @since 		1.0.0
+ * @version     1.0.0
+ */
+// Exit if accessed directly.
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+?>
 <p>Hello [display_name],</p><p>You have successfully changed your password. Click this link  [site_url] to login to your [blogname] account.</p>
 <p>Sincerely, <br/>
 [blogname]</p>
