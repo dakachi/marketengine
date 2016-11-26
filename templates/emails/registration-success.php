73704f037d99a61cd6c8519932f80f192894b83f
Ky Nguyen <kynh@youngworld.vn>
2016-11-26 10:26:00 +0700

    update email templates, order details and sample listing

--- templates/emails/registration-success.php
+++ templates/emails/registration-success.php
@@ -1,3 +1,24 @@
+<?php
+/**
+ * The email template for sending to user when they create an account.
+ *
+ * This template can be overridden by copying it to yourtheme/marketengine/emails/registration-success.php.
+ *
+ * @author 		EngineThemes
+ * @package 	MarketEngine/Templates
+ *
+ * @since 		1.0.0
+ *
+ * @version     1.0.0
+ *
+ */
+// Exit if accessed directly.
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+?>
+
 <p>Hello [display_name],</p>
 <p>You have successfully registered an account with [blogname].Here is your account information:</p>
 <ol>
