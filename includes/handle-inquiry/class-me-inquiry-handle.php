0ab7840533672766e72add466ee331592f379091
dakachi <david87dang@gmail.com>
2016-11-29 16:31:59 +0700

    fix bug load more after search contact in inquiry details

--- includes/handle-inquiry/class-me-inquiry-handle.php
+++ includes/handle-inquiry/class-me-inquiry-handle.php
@@ -172,7 +172,6 @@ class ME_Inquiry_Handle
             ));
 
             $users_found = $users_1->get_results();
-            var_dump($users_found);
             // no contact found
             if (empty($users_found)) {
                 ob_start();
