bac47cd2ca3378834030a2a8cabc5dd802b935f1
kn-youngworld <kynh@youngworld.vn>
2016-11-30 15:01:44 +0700

    add comment to send message form

--- templates/inquiry/send-message-disabled.php
+++ templates/inquiry/send-message-disabled.php
@@ -1,4 +1,18 @@
-
+<?php
+/**
+ * The template for displaying the disabled form of conversation page.
+ *
+ * This template can be override by copying it to
+ * yourtheme/marketengine/inquiry/send-message-disabled.php.
+ *
+ * @author EngineThemes
+ * @package MarketEngine/Templates
+ * @since 1.0.0
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+?>
 <div class="me-mc-container" id="me-mc-container"></div>
 <textarea disabled="disabled" id="me-message-content" class="required me-message-content" required name="content" placeholder="<?php _e("Type your message here", "enginethemes"); ?>"></textarea>
 <div class="upload-container">
--- templates/inquiry/send-message-form.php
+++ templates/inquiry/send-message-form.php
@@ -1,3 +1,18 @@
+<?php
+/**
+ * The template for displaying the form to send message.
+ *
+ * This template can be override by copying it to
+ * yourtheme/marketengine/inquiry/send-message-form.php.
+ *
+ * @author EngineThemes
+ * @package MarketEngine/Templates
+ * @since 1.0.0
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+?>
 <form method="post" id="send-message">
 	<div class="me-mc-container" id="me-mc-container"></div>
 	<textarea id="me-message-content" class="required me-message-content" required name="content" placeholder="<?php _e("Type your message here", "enginethemes"); ?>"></textarea>
