dc309f49daed9994d2492dbc63a2a92fd1990fa3
thoitranyw <thoitq@youngworld.vn>
2016-12-02 13:40:26 +0700

    style order item archive

--- templates/account/buyer-inquiry-list.php
+++ templates/account/buyer-inquiry-list.php
@@ -64,12 +64,12 @@ $query = new ME_Message_Query($args);
 				<?php endif; ?>
 			<?php else : ?>
 				<div class="me-table-col me-order-status">
-					<p class="me-item-archive"><i class="icon-me-info-circle"></i><?php _e('Archived', 'enginethemes'); ?></p>
+					<p class="me-order-item-archive"><i class="icon-me-info-circle"></i><?php _e('Archived', 'enginethemes'); ?></p>
 				</div>
 			<?php endif; ?>
 		<?php else : ?>
 			<div class="me-table-col me-order-status">
-				<p class="me-item-archive"><i class="icon-me-info-circle"></i><?php _e('Deleted', 'enginethemes'); ?></p>
+				<p class="me-order-item-archive"><i class="icon-me-info-circle"></i><?php _e('Deleted', 'enginethemes'); ?></p>
 			</div>
 		<?php endif; ?>
 
