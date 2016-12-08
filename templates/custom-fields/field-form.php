9937501ab9938e298765a75e4a8a33b77532955e
dakachi <david87dang@gmail.com>
2016-12-08 13:54:56 +0700

    add template post listing form custom field

---
+++ templates/custom-fields/field-form.php
@@ -0,0 +1,82 @@
+<div class="marketengine-custom-field">
+	<div class="marketengine-group-field">
+		<div class="marketengine-input-field">
+		    <label for="me_cf_text_1" class="me-field-title">Custom field text</label>
+		    <input id="me_cf_text_1" type="text" placeholder="Text placeholder" name="me_cf_text_1">
+		</div>
+	</div>
+
+	<div class="marketengine-group-field">
+		<div class="marketengine-textarea-field">
+		    <label for="me_cf_textarea_2" class="me-field-title">Custom field textarea</label>
+		    <textarea id="me_cf_textarea_2" placeholder="Textarea placeholder" name="me_cf_textarea_2"></textarea>
+		</div>
+	</div>
+
+	<div class="marketengine-group-field">
+		<div class="marketengine-input-field">
+		    <label for="me_cf_number_3" class="me-field-title">Custom field number <small>(optional)</small><i class="me-help-text icon-me-question-circle" title="Help text: Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt"></i></label>
+		    <input id="me_cf_number_3" type="number" placeholder="Number placeholder" name="me_cf_number_3" min="1" max="1001">
+		</div>
+	</div>
+	
+	<div class="marketengine-group-field">
+		<div class="marketengine-date-field">
+		    <label for="me_cf_date_4" class="me-field-title">Custom field date</label>
+		    <input id="me_cf_date_4" type="text" placeholder="Date placeholder" name="me_cf_date_4">
+		    <i class="icon-me-calendar"></i>
+		</div>
+	</div>
+
+	<div class="marketengine-group-field">
+		<div class="marketengine-checkbox-field">
+		    <label class="me-field-title">Custom field Checkbox</label>
+		    <div class="me-checkbox">
+		    	<label for="me_cf_checkbox_5"><input id="me_cf_checkbox_5" type="checkbox"><span>Option 1</span></label>	
+		    </div>
+		    <div class="me-checkbox">
+		    	<label for="me_cf_checkbox_6"><input id="me_cf_checkbox_6" type="checkbox"><span>Option 101</span></label>	
+		    </div>
+		    <div class="me-checkbox">
+		    	<label for="me_cf_checkbox_7"><input id="me_cf_checkbox_7" type="checkbox"><span>Option 1001 overnight full options</span></label>
+		    </div>
+		    
+		</div>
+	</div>
+	<div class="marketengine-group-field">
+		<div class="marketengine-radio-field">
+		    <label class="me-field-title">Custom field Checkbox</label>
+		    <div>
+		    	<label class="me-radio" for="me_cf_radio_8"><input id="me_cf_radio_8" name="me_cf_radio" type="radio"><span>Option 1</span></label>	
+			    <label class="me-radio" for="me_cf_radio_9"><input id="me_cf_radio_9" name="me_cf_radio" type="radio"><span>Option 101</span></label>	
+			   	<label class="me-radio" for="me_cf_radio_10"><input id="me_cf_radio_10" name="me_cf_radio" type="radio"><span>Option 1001 overnight full options</span></label>
+			</div>
+		</div>
+	</div>
+
+	<div class="marketengine-group-field">
+		<div class="marketengine-input-field">
+		    <label for="me_cf_date_4" class="me-field-title">Custom field single select</label>
+		    <select name="" id="" class="me-chosen-select me-cf-chosen">
+		    	<option value="">Single select 1</option>
+		    	<option value="">Single select 2</option>
+		    	<option value="">Single select 3</option>
+		    	<option value="">Single select 4</option>
+		    </select>
+		</div>
+	</div>
+
+
+	<div class="marketengine-group-field">
+		<div class="marketengine-input-field">
+		    <label for="me_cf_date_4" class="me-field-title">Custom field single select</label>
+		    <select name="" id="" class="me-chosen-select me-cf-chosen" multiple="">
+		    	<option value="">Single select 1</option>
+		    	<option value="">Single select 2</option>
+		    	<option value="">Single select 3</option>
+		    	<option value="">Single select 4</option>
+		    </select>
+		</div>
+	</div>
+
+</div>
