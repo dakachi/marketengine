e90d7623d67cb5f23df08e4323a38eed7890dbc8
dakachi <david87dang@gmail.com>
2016-12-12 14:40:21 +0700

    update date field in listing details

--- templates/custom-fields/field-details.php
+++ templates/custom-fields/field-details.php
@@ -12,7 +12,7 @@ foreach ($fields as $field) :
 		case 'text':
 		case 'textarea':
 		case 'number' :
-		case 'date' :
+		
 		$value = me_field($field['field_name']);
 		if(!$value) break;
 		?>
@@ -31,7 +31,27 @@ foreach ($fields as $field) :
 		</div>
 		<?php
 			break;
+		case 'date' : 
+		$value = me_field($field['field_name']);
+		if(!$value) break;
 
+		$date = date_i18n( get_option('date_format'), strtotime($value) );
+
+		?>
+		<div class="me-row">
+			<div class="me-col-sm-3">
+				<div class="me-cf-title">
+					<p><?php echo $field['field_title'] ?></p>
+					<span><?php echo $field['field_description'] ?></span>
+				</div>
+			</div>
+			<div class="me-col-sm-9">
+				<div class="me-cf-content">
+					<p><?php echo $date; ?></p>
+				</div>
+			</div>
+		</div>
+		<?php
 		case 'radio':
 		case 'checkbox':
 		case 'select': 
