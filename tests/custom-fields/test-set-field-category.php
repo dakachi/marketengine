e6a728fdb04be437647281fc30cf0e9fe15343c2
dakachi <david87dang@gmail.com>
2016-12-13 18:04:31 +0700

    update test set category

--- tests/custom-fields/test-set-field-category.php
+++ tests/custom-fields/test-set-field-category.php
@@ -72,6 +72,10 @@ class Tests_Set_Field_Category extends WP_UnitTestCase
         me_cf_set_field_category($this->field_id, $this->parent_cat, 1);
         $fields = me_cf_get_fields($this->parent_cat);
         $field = array_pop($fields);
+
+        $count = get_term_meta( $this->parent_cat, '_me_cf_count', true );
+
         $this->assertEquals($this->field_id, $field['field_id']);
+        $this->assertEquals(1, $count);
     }
 }
