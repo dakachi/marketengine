a20dc4197a2893828a76ee505010cd93f0072b26
dakachi <david87dang@gmail.com>
2016-12-14 18:01:11 +0700

    add me-field-taxonomy

---
+++ includes/custom-fields/me-field-taxonomy.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Fields are predefined and user only selected available values. Using wordpress taxonomy to control the field value
+ */
+// Exit if accessed directly.
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+// function me_cf_register_field_taxonomy()
+// {
+
+// }
+
+// function me_cf_add_field_taxonomy_option()
+// {
+
+// }
+
+// dang ky taxonomy, luu y taxonomy se la build-in, ko show trong menu
+// luu option tu add field, update field
+// get field option
+// ... co the co nhieu ham bo tro nua
