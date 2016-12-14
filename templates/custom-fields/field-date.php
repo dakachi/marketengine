331269541ce6fba8aa1ed7339b4b993f8cce366a
dakachi <david87dang@gmail.com>
2016-12-14 10:50:16 +0700

    update field validator

--- templates/custom-fields/field-date.php
+++ templates/custom-fields/field-date.php
@@ -8,7 +8,7 @@
 
 	    	<i class="me-help-text icon-me-question-circle" title="<?php echo $field['field_help_text']; ?>"></i>
 	    </label>
-	    <input <?php echo me_field_attribute($field); ?> id="<?php echo $field['field_name'] ?>" type="text" placeholder="<?php echo $field['field_placeholder'] ?>" name="<?php echo $field['field_name'] ?>" value="<?php echo $value; ?>">
+	    <input <?php //echo me_field_attribute($field); ?> id="<?php echo $field['field_name'] ?>" type="text" placeholder="<?php echo $field['field_placeholder'] ?>" name="<?php echo $field['field_name'] ?>" value="<?php echo $value; ?>">
 	    <i class="icon-me-calendar"></i>
 	</div>
 </div>
