9b0522bccc607ee85fe5290edff11dfe0e63afb1
dakachi <david87dang@gmail.com>
2016-12-14 14:03:59 +0700

    update field lable

--- templates/custom-fields/field-text.php
+++ templates/custom-fields/field-text.php
@@ -7,7 +7,10 @@
 	    	<small><?php _e("(optional)", "enginethemes"); ?></small>
 	    	<?php endif; ?>
 
-	    	<i class="me-help-text icon-me-question-circle" title="<?php echo $field['field_help_text']; ?>"></i>
+	    	<?php if($field['field_help_text']) : ?>
+	    		<i class="me-help-text icon-me-question-circle" title="<?php echo $field['field_help_text']; ?>"></i>
+	    	<?php endif; ?>
+	    	
 	    </label>
 	    <input <?php //echo me_field_attribute($field); ?> id="<?php echo $field['field_name'] ?>" type="text" placeholder="<?php echo $field['field_placeholder'] ?>" name="<?php echo $field['field_name'] ?>" value="<?php echo $value; ?>" >
 	</div>
