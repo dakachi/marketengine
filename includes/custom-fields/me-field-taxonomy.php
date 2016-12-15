16fe654f760bb088e8f50e9dd93153f2708f0385
dakachi <david87dang@gmail.com>
2016-12-15 17:23:20 +0700

    update me field taxonomy

--- includes/custom-fields/me-field-taxonomy.php
+++ includes/custom-fields/me-field-taxonomy.php
@@ -19,5 +19,6 @@ if (!defined('ABSPATH')) {
 
 // dang ky taxonomy, luu y taxonomy se la build-in, ko show trong menu
 // luu option tu add field, update field
-// get field option
+// get field option tạo format $key : $value ( trong backend )
+// get field option format $key => $value (ở front-end)
 // ... co the co nhieu ham bo tro nua
