0db6cf784fd84855977537a6737d32234a983c83
dakachi <david87dang@gmail.com>
2016-12-22 14:34:01 +0700

    add validate constraint to field

--- includes/custom-fields/class-me-handle-cf.php
+++ includes/custom-fields/class-me-handle-cf.php
@@ -137,6 +137,10 @@ class ME_Handle_CF
                 $rules[$field_name] .= '|date';
             }
 
+            if ($field['field_type'] == 'number') {
+                $rules[$field_name] .= '|numeric';
+            }
+
             $custom_attributes[$field_name] = $field['field_title'];
         }
 
