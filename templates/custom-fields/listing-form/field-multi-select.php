cf051fa0944a81f65336d1bef386905b66b67c9b
dakachi <david87dang@gmail.com>
2016-12-22 10:25:37 +0700

    remove multiselect field option none

--- templates/custom-fields/listing-form/field-multi-select.php
+++ templates/custom-fields/listing-form/field-multi-select.php
@@ -22,11 +22,7 @@ if(empty($options)) return;
 <div class="marketengine-group-field">
 	<div class="marketengine-input-field">
 	    <?php me_get_template('custom-fields/listing-form/field-label', array('field' => $field));  ?>
-	    <select name="<?php echo $field['field_name'] ?>[]" id="<?php echo $field['field_name'] ?>" class="me-chosen-select me-cf-chosen" multiple="true">
-	    	<?php if( $field['field_placeholder'] ) : ?>
-	    		<option value=""><?php echo $field['field_placeholder']; ?></option>
-	    	<?php endif; ?>	
-
+	    <select name="<?php echo $field['field_name'] ?>[]" id="<?php echo $field['field_name'] ?>" class="me-chosen-select me-cf-chosen" multiple="true"  data-placeholder="<?php echo $field['field_placeholder']; ?>">
 	    	<?php foreach ($options as $option) : ?>
 	    		<option value="<?php echo $option['value'] ?>" <?php if(in_array($option['value'], (array)$value)) {echo 'selected="true"';} ?>>
 	    			<?php echo $option['label']; ?>
