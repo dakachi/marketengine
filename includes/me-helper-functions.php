0251faf52dc93dc88c54326df48baf7f71f80404
dakachi <david87dang@gmail.com>
2016-12-26 09:53:22 +0700

    update helper function

--- includes/me-helper-functions.php
+++ includes/me-helper-functions.php
@@ -69,3 +69,121 @@ function me_default_endpoints()
     );
     return $endpoint_arr;
 }
+
+function me_get_option($option, $default = '')
+{
+    return get_option($option, $default);
+}
+
+function me_option($option, $default = '')
+{
+    $options = ME_Options::get_instance();
+    return $options->get_option($option, $default);
+}
+
+function me_update_option($option, $value)
+{
+    $options = ME_Options::get_instance();
+    return $options->update_option($option, $value);
+}
+
+function me_get_client_ip()
+{
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else {
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
+
+function me_get_client_agent()
+{
+    return !empty($_SERVER['HTTP_USER_AGENT']) ? strtolower($_SERVER['HTTP_USER_AGENT']) : '';
+}
+
+/**
+ * Trims text to a certain number of words.
+ *
+ * This function is localized. For languages that count 'words' by the individual
+ * character (such as East Asian languages), the $num_words argument will apply
+ * to the number of individual characters.
+ *
+ * @since 1.0
+ *
+ * @param string $text      Text to trim.
+ * @param int    $num_words Number of words. Default 55.
+ * @param string $more      Optional. What to append if $text needs to be trimmed. Default '&hellip;'.
+ * @return string Trimmed text.
+ */
+function me_trim_words($text, $num_words = 55, $more = null)
+{
+    if (null === $more) {
+        $more = __('&hellip;');
+    }
+
+    $original_text = $text;
+    //$text = wp_strip_all_tags( $text );
+
+    /*
+     * translators: If your word count is based on single characters (e.g. East Asian characters),
+     * enter 'characters_excluding_spaces' or 'characters_including_spaces'. Otherwise, enter 'words'.
+     * Do not translate into your own language.
+     */
+    if (strpos(_x('words', 'Word count type. Do not translate!'), 'characters') === 0 && preg_match('/^utf\-?8$/i', get_option('blog_charset'))) {
+        //$text = trim( preg_replace( "/[\n\r\t ]+/", ' ', $text ), ' ' );
+        //preg_match_all( '/./u', $text, $words_array );
+        $words_array = array_slice($words_array[0], 0, $num_words + 1);
+        $sep         = '';
+    } else {
+        $words_array = preg_split("/[\n\r\t ]+/", $text, $num_words + 1, PREG_SPLIT_NO_EMPTY);
+        $sep         = ' ';
+    }
+
+    if (count($words_array) > $num_words) {
+        array_pop($words_array);
+        $text = implode($sep, $words_array);
+        $text = $text . $more;
+    } else {
+        $text = implode($sep, $words_array);
+    }
+
+    /**
+     * Filters the text content after words have been trimmed.
+     *
+     * @since 3.3.0
+     *
+     * @param string $text          The trimmed text.
+     * @param int    $num_words     The number of words to trim the text to. Default 5.
+     * @param string $more          An optional string to append to the end of the trimmed text, e.g. &hellip;.
+     * @param string $original_text The text before it was trimmed.
+     */
+    return apply_filters('me_trim_words', $text, $num_words, $more, $original_text);
+}
+
+/**
+ * Format file size Unit
+ *
+ * @param int $bytes
+ * @return string
+ */
+function me_format_size_units($bytes)
+{
+    if ($bytes >= 1073741824) {
+        $bytes = number_format($bytes / 1073741824, 2) . ' GB';
+    } elseif ($bytes >= 1048576) {
+        $bytes = number_format($bytes / 1048576, 2) . ' MB';
+    } elseif ($bytes >= 1024) {
+        $bytes = number_format($bytes / 1024, 2) . ' kB';
+    } elseif ($bytes > 1) {
+        $bytes = $bytes . ' bytes';
+    } elseif ($bytes == 1) {
+        $bytes = $bytes . ' byte';
+    } else {
+        $bytes = '0 bytes';
+    }
+
+    return $bytes;
+}
--- includes/me-template-functions.php
+++ includes/me-template-functions.php
@@ -230,7 +230,7 @@ function me_post_tags_meta_box($default, $taxonomy)
 }
 
 /**
- * MarketEngine Paginate Link
+ * MarketEngine Paginate Link Template
  *
  * @since 1.0
  */
@@ -269,42 +269,6 @@ function marketengine_sidebar()
     me_get_template('global/sidebar');
 }
 add_action('marketengine_sidebar', 'marketengine_sidebar');
-
-// TODO: tam thoi de day
-function me_get_option($option, $default = '')
-{
-    return get_option($option, $default);
-}
-
-function me_option($option, $default = '')
-{
-    $options = ME_Options::get_instance();
-    return $options->get_option($option, $default);
-}
-
-function me_update_option($option, $value)
-{
-    $options = ME_Options::get_instance();
-    return $options->update_option($option, $value);
-}
-
-// TODO: noi bo ham nay
-function me_get_client_ip()
-{
-    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-        $ip = $_SERVER['HTTP_CLIENT_IP'];
-    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    } else {
-        $ip = $_SERVER['REMOTE_ADDR'];
-    }
-    return $ip;
-}
-
-function me_get_client_agent()
-{
-    return !empty($_SERVER['HTTP_USER_AGENT']) ? strtolower($_SERVER['HTTP_USER_AGENT']) : '';
-}
 
 /**
  * Returns url of user account page or an its endpoint.
@@ -727,91 +691,7 @@ function marketengine_get_the_archive_title()
 }
 
 /**
- * Trims text to a certain number of words.
- *
- * This function is localized. For languages that count 'words' by the individual
- * character (such as East Asian languages), the $num_words argument will apply
- * to the number of individual characters.
- *
- * @since 1.0
- *
- * @param string $text      Text to trim.
- * @param int    $num_words Number of words. Default 55.
- * @param string $more      Optional. What to append if $text needs to be trimmed. Default '&hellip;'.
- * @return string Trimmed text.
- */
-function me_trim_words($text, $num_words = 55, $more = null)
-{
-    if (null === $more) {
-        $more = __('&hellip;');
-    }
-
-    $original_text = $text;
-    //$text = wp_strip_all_tags( $text );
-
-    /*
-     * translators: If your word count is based on single characters (e.g. East Asian characters),
-     * enter 'characters_excluding_spaces' or 'characters_including_spaces'. Otherwise, enter 'words'.
-     * Do not translate into your own language.
-     */
-    if (strpos(_x('words', 'Word count type. Do not translate!'), 'characters') === 0 && preg_match('/^utf\-?8$/i', get_option('blog_charset'))) {
-        //$text = trim( preg_replace( "/[\n\r\t ]+/", ' ', $text ), ' ' );
-        //preg_match_all( '/./u', $text, $words_array );
-        $words_array = array_slice($words_array[0], 0, $num_words + 1);
-        $sep         = '';
-    } else {
-        $words_array = preg_split("/[\n\r\t ]+/", $text, $num_words + 1, PREG_SPLIT_NO_EMPTY);
-        $sep         = ' ';
-    }
-
-    if (count($words_array) > $num_words) {
-        array_pop($words_array);
-        $text = implode($sep, $words_array);
-        $text = $text . $more;
-    } else {
-        $text = implode($sep, $words_array);
-    }
-
-    /**
-     * Filters the text content after words have been trimmed.
-     *
-     * @since 3.3.0
-     *
-     * @param string $text          The trimmed text.
-     * @param int    $num_words     The number of words to trim the text to. Default 5.
-     * @param string $more          An optional string to append to the end of the trimmed text, e.g. &hellip;.
-     * @param string $original_text The text before it was trimmed.
-     */
-    return apply_filters('me_trim_words', $text, $num_words, $more, $original_text);
-}
-
-/**
- * Format file size Unit
- *
- * @param int $bytes
- * @return string
- */
-function me_format_size_units($bytes)
-{
-    if ($bytes >= 1073741824) {
-        $bytes = number_format($bytes / 1073741824, 2) . ' GB';
-    } elseif ($bytes >= 1048576) {
-        $bytes = number_format($bytes / 1048576, 2) . ' MB';
-    } elseif ($bytes >= 1024) {
-        $bytes = number_format($bytes / 1024, 2) . ' kB';
-    } elseif ($bytes > 1) {
-        $bytes = $bytes . ' bytes';
-    } elseif ($bytes == 1) {
-        $bytes = $bytes . ' byte';
-    } else {
-        $bytes = '0 bytes';
-    }
-
-    return $bytes;
-}
-
-/**
- * Change title of page if user access their manage pages
+ * Hook action `the_title` to change title of page if user access their manage pages
  *
  * @param string $title, int $id
  * @return string
