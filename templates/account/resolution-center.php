f4e3e4c895da8389182aab8b89f605ff30189a68
kn-youngworld <kynh@youngworld.vn>
2016-12-27 11:37:18 +0700

    add resolution center template

---
+++ templates/account/resolution-center.php
@@ -0,0 +1,18 @@
+<div class="me-resolution">
+	<?php me_get_template('resolution/resolution-filter'); ?>
+
+	<?php me_get_template('resolution/dispute-case-list'); ?>
+
+	<div class="me-paginations">
+		<div>
+			<a class="prev page-numbers" href="#">&lt;</a>
+			<a class="page-numbers" href="#">1</a>
+			<span class="page-numbers current">2</span>
+			<a class="page-numbers" href="#">3</a>
+			<a class="next page-numbers" href="">&gt;</a>
+		</div>
+	</div>
+	<div class="marketengine-loadmore">
+		<a href="" class="me-loadmore me-loadmore-order"><?php _e('Load more', 'enginethemes'); ?></a>
+	</div>
+</div>
---
+++ templates/resolution/dispute-case-list.php
@@ -0,0 +1,7 @@
+<div class="me-table me-table-resolution">
+
+	<?php me_get_template('resolution/dispute-case-list-header'); ?>
+
+	<?php me_get_template('resolution/dispute-case-list-rows'); ?>
+
+</div>
