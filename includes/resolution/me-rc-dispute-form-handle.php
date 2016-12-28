ddc0148ec6b8bb9a37f185c6c11e60e046f8c004
kn-youngworld <kynh@youngworld.vn>
2016-12-28 14:45:49 +0700

    add dispute form handle

---
+++ includes/resolution/me-rc-dispute-form-handle.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+* MarketEngine Dispute Form Handle Class
+*
+* @author 		EngineThemes
+* @package 		MarketEngine/Includes
+*/
+class ME_Dispute_Form {
+	public static function create_dispute_case($case_data) {
+
+	}
+}
