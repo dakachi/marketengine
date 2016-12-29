8b7b51c2ef1e988a509dc97824c4c12b06a80d43
kn-youngworld <kynh@youngworld.vn>
2016-12-29 13:15:42 +0700

    update dispute form

--- templates/resolution/dispute-form.php
+++ templates/resolution/dispute-form.php
@@ -14,28 +14,26 @@
 
 <div class="me-dispute-form">
 
+	<?php me_print_notices(); ?>
+
 	<?php do_action('marketengine_start_dispute_form'); ?>
 
-	<form id="me-dispute-form" method="post" action="">
+	<form id="me-dispute-form" method="post" action="" enctype="multipart/form-data">
 
 		<?php me_get_template('resolution/disputed-product-info', array('transaction' => $transaction)); ?>
 
-		<div class="me-receive-item">
-			<h3><?php _e('Did you receive your item?', 'enginethemes'); ?></h3>
-			<label for="me-receive-item-yes"><input class="me-receive-item-field" id="me-receive-item-yes" type="radio" data-get-refund-block="dispute-get-refund-yes" name="me-receive-item" checked><?php _e('Yes', 'enginethemes'); ?></label>
-			<label for="me-receive-item-no"><input class="me-receive-item-field" id="me-receive-item-no" type="radio" data-get-refund-block="dispute-get-refund-no" name="me-receive-item"><?php _e('No', 'enginethemes'); ?></label>
-		</div>
+		<?php me_get_template('resolution/dispute-received-item'); ?>
 
 		<?php me_get_template('resolution/dispute-problem'); ?>
 
 		<div class="me-dispute-description">
 			<h3><?php _e('Please tell more about your problem', 'enginethemes'); ?></h3>
-			<textarea name="dispute-description"></textarea>
+			<textarea name="me-dispute-problem-description"><?php echo isset($_POST['me-dispute-problem-description']) ? esc_js($_POST['me-dispute-problem-description']) : ''; ?></textarea>
 		</div>
 
 		<div class="me-dispute-image">
 			<h3><?php _e('Attachments (video or images)', 'enginethemes'); ?></h3>
-			<!--
+			<?php /*
 			<ul class="marketengine-gallery-img">
 				<li class="me-item-img">
 					<span class="me-gallery-img">
@@ -66,13 +64,13 @@
 					</span>
 				</li>
 			</ul>
-			-->
+			*/ ?>
 			<ul class="marketengine-gallery-img">
 				<li class="me-item-img">
 					<span class="me-gallery-img me-gallery-add-img">
 						<a class="me-add-img">
 							<i class="icon-me-add"></i>
-							<input type="file" name="dispute-media" value="">
+							<input type="file" name="me-dispute-media" value="">
 						</a>
 					</span>
 				</li>
@@ -81,10 +79,16 @@
 
 		<?php me_get_template('resolution/expected-resolution'); ?>
 
+		<?php wp_nonce_field('me-open_dispute_case'); ?>
+
 		<div class="me-dispute-submit">
-			<input type="submit" class="me-dispute-submit-btn" value="<?php _e('SUBMIT', 'enginethemes'); ?>">
+			<input type="submit" class="me-dispute-submit-btn" name="me-open-dispute-case" value="<?php _e('SUBMIT', 'enginethemes'); ?>">
 		</div>
 		<a href="<?php echo remove_query_arg('action'); ?>" class="me-backlink"><?php _e('&lt; Back to transaction details', 'enginethemes'); ?></a>
+
+		<input type="hidden" name="transaction-id" value="<?php echo $transaction->id; ?>">
+		<input type="hidden" name="redirect" value="<?php echo get_the_permalink($transaction->id); ?>">
+
 	</form>
 
 	<?php do_action('marketengine_end_dispute_form'); ?>
