46d6ebcec05e29e1178516f209061e6e62d03a4d
dakachi <david87dang@gmail.com>
2016-12-29 11:33:33 +0700

    update order resolution link

--- templates/resolution/dispute-button.php
+++ templates/resolution/dispute-button.php
@@ -1,23 +1,16 @@
 <?php
-	$dispute_time_limit = $transaction->get_dispute_time_limit() ;
+// Exit if accessed directly.
+if (!defined('ABSPATH')) {
+    exit;
+}
 ?>
 
-<?php if( 'me-disputed' === $transaction->post_status) : ?>
+<div class="me-transaction-dispute">
+	<p>
+		<?php printf( __('You have %s to dispute this order.', 'enginethemes'), $dispute_time_limit); ?>
+	</p>
+	<a href="<?php echo me_get_page_permalink('dispute'); ?>" >
+		<?php _e('DISPUTE', 'enginethemes'); ?>
+	</a>
 
-	<div class="me-orderwarning-info">
-		<p><?php echo __('This order has been disputed by buyer.', 'enginethemes'); ?></p>
-		<a href="#" class="me-resolution-center"><?php _e('TO RESOLUTION CENTER', 'enginethemes'); ?></a>
-	</div>
-
-<?php elseif ('me-pending' !== $transaction->post_status && $dispute_time_limit) : ?>
-
-	<div class="me-transaction-dispute">
-		<p>
-			<?php printf( __('You have %s to dispute this order.', 'enginethemes'), $dispute_time_limit); ?>
-		</p>
-		<a href="<?php echo me_get_page_permalink('dispute'); ?>" class="">
-			<?php _e('DISPUTE', 'enginethemes'); ?>
-		</a>
-
-	</div>
-<?php endif; ?>
+</div>
