3928928b2d0c2f25aa8c0765e7794c45123905fe
dakachi <david87dang@gmail.com>
2016-12-29 11:15:18 +0700

    add dispute button

--- includes/resolution/me-rc-template-functions.php
+++ includes/resolution/me-rc-template-functions.php
@@ -1,1 +1,9 @@
 <?php
+// Exit if accessed directly.
+if (!defined('ABSPATH')) {
+    exit;
+}
+function me_rc_button($transaction) {
+	me_get_template('resolution/dispute-button', array('transaction' => $transaction));
+}
+add_action( 'marketengine_order_extra_content', 'me_rc_button', 11);
--- templates/resolution/dispute-button.php
+++ templates/resolution/dispute-button.php
@@ -1,18 +1,20 @@
-<?php if( 'me-disputed' === $order->post_status) : ?>
+<?php
+	$dispute_time_limit = $transaction->get_dispute_time_limit() ;
+?>
+
+<?php if( 'me-disputed' === $transaction->post_status) : ?>
 
 <div class="me-orderwarning-info">
 	<p><?php echo __('This order has been disputed by buyer.', 'enginethemes'); ?></p>
 	<a href="#" class="me-resolution-center"><?php _e('TO RESOLUTION CENTER', 'enginethemes'); ?></a>
 </div>
 
-<?php elseif ('me-pending' !== $order->post_status ) : ?>
+<?php elseif ('me-pending' !== $transaction->post_status && $dispute_time_limit > 0 ) : ?>
 
 <div class="me-transaction-dispute">
-	<?php
-		$time_limit = $order->get_dispute_time_limit();
-	?>
 
-	<p><?php printf( __('You have %s to dispute this order.', 'enginethemes'), sprintf(_n('%d day', '%d days', $time_limit, 'enginethemes'), $time_limit) ); ?></p>
+	<p>
+		<?php printf( __('You have %s to dispute this order.', 'enginethemes'), $dispute_time_limit); ?></p>
 	<a href="<?php echo me_get_option_page_id('dispute'); ?>" class=""><?php _e('DISPUTE', 'enginethemes'); ?></a>
 
 </div>
