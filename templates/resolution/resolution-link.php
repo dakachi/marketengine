868f4bcf5db4e0c2392c81251d0d90e741556f40
dakachi <david87dang@gmail.com>
2016-12-29 11:54:22 +0700

    update resolution link

--- templates/resolution/resolution-link.php
+++ templates/resolution/resolution-link.php
@@ -1,4 +1,4 @@
 <div class="me-orderwarning-info">
-	<p><?php echo __('This order has been disputed.', 'enginethemes'); ?></p>
+	<p><?php echo __('This order has been disputed. In order to resolve problems, move to resolution center', 'enginethemes'); ?></p>
 	<a href="#" class="me-resolution-center"><?php _e('TO RESOLUTION CENTER', 'enginethemes'); ?></a>
 </div>
