2975e3598244385007ff8bfeb0a340439ab53a6b
dakachi <david87dang@gmail.com>
2017-01-04 15:10:08 +0700

    update resolution details

--- templates/resolution/me-single-case.php
+++ templates/resolution/me-single-case.php
@@ -206,24 +206,19 @@ $item = array_pop($items);
                                             </li>
                                         </ul>
                                     </div>
+                                    <?php 
+$message_query = new ME_Message_Query(array('post_type' => 'message', 'post_parent' => $case->ID, 'showposts' => 12));
+	$messages = array_reverse ($message_query->posts);
+?>
+                                    
                                     <div class="me-contact-messages">
                                         <ul class="me-contact-messages-list">
-                                            <li>
-                                                <span class="me-mgs-author-avatar">
-                                                    <img alt="" src="../assets/img/avatar.png"/>
-                                                </span>
-                                                <div class="me-message-author">
-                                                    <h4 class="me-author">
-                                                        David Copperfield
-                                                    </h4>
-                                                    <p>
-                                                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui.
-                                                    </p>
-                                                    <span>
-                                                        2 hours ago
-                                                    </span>
-                                                </div>
-                                            </li>
+                                        <?php if( $messages ) : ?>
+										<?php foreach ($messages  as $key => $message) : ?>
+											<?php me_get_template('resolution/message-item', array('message' => $message)); ?>
+										<?php endforeach; ?>
+										<?php endif; ?>
+                                            
                                         </ul>
                                     </div>
                                     <div class="me-message-typing">
