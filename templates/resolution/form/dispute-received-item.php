4d0e84bed7870fe320fb1d27a56e0f39cdb7c8c3
thoitranyw <thoitq@youngworld.vn>
2017-01-04 17:34:37 +0700

    fix style dispute form

--- templates/resolution/form/dispute-received-item.php
+++ templates/resolution/form/dispute-received-item.php
@@ -4,13 +4,16 @@
 
 <div class="me-receive-item">
 	<h3><?php _e('Did you receive your item?', 'enginethemes'); ?></h3>
-	<label for="me-receive-item-yes">
-		<input class="me-receive-item-field" id="me-receive-item-yes" type="radio" data-get-refund-block="dispute-get-refund-yes" name="is_received_item" value="true" <?php checked($checked, true); ?>>
-		<?php _e('Yes', 'enginethemes'); ?>
-	</label>
-
-	<label for="me-receive-item-no">
-		<input class="me-receive-item-field" id="me-receive-item-no" type="radio" data-get-refund-block="dispute-get-refund-no" name="is_received_item" value="false" <?php checked($checked, false); ?>>
-		<?php _e('No', 'enginethemes'); ?>
-	</label>
+	<div class="marketengine-radio-field">
+	    <div>
+	    	<label class="me-radio" for="me-receive-item-yes">
+	    		<input class="me-receive-item-field" id="me-receive-item-yes" name="is_received_item" type="radio" data-get-refund-block="dispute-get-refund-yes" value="true" <?php checked($checked, true); ?>>
+	    		<span><?php _e('Yes', 'enginethemes'); ?></span>
+	    		</label>	
+		    <label class="me-radio" for="me-receive-item-no">
+		    	<input class="me-receive-item-field" id="me-receive-item-no" name="is_received_item" type="radio" data-get-refund-block="dispute-get-refund-no" value="false" <?php checked($checked, false); ?>>
+		    	<span><?php _e('No', 'enginethemes'); ?></span>
+	    	</label>	
+		</div>
+	</div>
 </div>
--- templates/resolution/form/resolution-item.php
+++ templates/resolution/form/resolution-item.php
@@ -1,8 +1,9 @@
 <?php foreach ($resolutions as $key => $resolution) : ?>
 
-<div class="dispute-get-refund">
-	<label for="<?php echo $key; ?>">
-		<input class="me-solution-item" id="<?php echo $key; ?>" type="radio" name="expect_solution" value="<?php echo $key; ?>" <?php checked(isset($_POST['expect_solution']) && $_POST['expect_solution'] == $key); ?>><?php echo $resolution['label']; ?>
+<div class="marketengine-radio-field dispute-get-refund">
+	<label class="me-radio" for="<?php echo $key; ?>">
+    	<input id="<?php echo $key; ?>" name="expect_solution" type="radio" value="<?php echo $key; ?>"<?php checked(isset($_POST['expect_solution']) && $_POST['expect_solution'] == $key); ?>>
+    	<span><?php echo $resolution['label']; ?></span>
 	</label>
 	<span><?php echo $resolution['description']; ?></span>
 </div>
