35037342ab9f5030305e8072abbd343965e273be
thoitranyw <thoitq@youngworld.vn>
2017-01-05 09:41:05 +0700

    fix resolution center

--- templates/resolution/cases/dispute-case-list.php
+++ templates/resolution/cases/dispute-case-list.php
@@ -12,7 +12,7 @@
 
 <div class="me-table me-table-resolution">
 
-	<?php me_get_template('resolution/cases-table-header'); ?>
+	<?php me_get_template('resolution/cases/cases-table-header'); ?>
 
 	<?php if($query->found_posts) : ?>
 
