f5eb1dc39c2268086a7a3d49e1b2bbdd661d43b2
dakachi <david87dang@gmail.com>
2017-01-05 16:11:57 +0700

    update revision item

--- templates/resolution/revision-item.php
+++ templates/resolution/revision-item.php
@@ -1,7 +1,22 @@
 <li class="me-contact-dispute-event">
-	<p>
-		<b><?php echo get_the_author_meta( 'display_name', $message->sender ); ?></b>
-		<i>has closed the dipute</i>
-	</p>
+<?php 
+switch ($message->post_status) :
+	case 'me-closed':
+		$name = get_the_author_meta( 'display_name', $message->sender );
+	?>
+		<p>			
+			<?php printf(__("<b>%s</b> <i>has closed the dipute</i>", "enginethemes"), $name); ?>
+		</p>
+	<?php
+		break;
+	case 'me-waiting':
+		$name = get_the_author_meta( 'display_name', $message->receiver );
+	?>
+		<p>			
+			<?php printf(__("<b>%s</b> <i>has requested to closed the dipute</i>", "enginethemes"), $name); ?>
+		</p>
+	<?php
+endswitch;
+ ?>
 	<span><?php echo date_i18n(get_option('date_format') .' ' . get_option('time_format') ,strtotime($message->post_date) ); ?></span>
 </li>
