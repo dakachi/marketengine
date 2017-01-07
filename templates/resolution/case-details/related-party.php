6132546d4a342b2187647753164e97dc81566dff
dakachi <david87dang@gmail.com>
2017-01-07 11:24:44 +0700

    update resolution center link template

--- templates/resolution/case-details/related-party.php
+++ templates/resolution/case-details/related-party.php
@@ -10,7 +10,7 @@ if (!defined('ABSPATH')) {
     <?php if(current_user_can( 'manage_options' ) || $case->sender == get_current_user_id()) : ?>
         <p>
             <?php _e("Seller:", "enginethemes"); ?>
-             <a href="<?php get_author_posts_url( $case->receiver ); ?>">
+             <a href="<?php echo get_author_posts_url( $case->receiver ); ?>">
                 <?php echo get_the_author_meta( 'display_name', $case->receiver ); ?>
             </a>
         </p>
@@ -20,7 +20,7 @@ if (!defined('ABSPATH')) {
         
         <p>
             <?php _e("Buyer:", "enginethemes"); ?>
-            <a href="<?php get_author_posts_url( $case->sender ); ?>">
+            <a href="<?php echo get_author_posts_url( $case->sender ); ?>">
                 <?php echo get_the_author_meta( 'display_name', $case->sender ); ?>
             </a>
         </p>
