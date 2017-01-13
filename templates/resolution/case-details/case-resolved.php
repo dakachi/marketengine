3d193d88b2c4fcf5a535036032efb482fbfdc280
dakachi <david87dang@gmail.com>
2017-01-13 16:27:01 +0700

    add template case resolved

---
+++ templates/resolution/case-details/case-resolved.php
@@ -0,0 +1,7 @@
+<h4>Super Buyer 1 wins the dispute</h4>
+<p>
+Admin arbitrates:<br>Due to the brief which Super Buyer 1 provides, the Admin considers that he is right in this situation. 
+Super Buyer 1 has to return the item right before xx days in order to get full refrund from the Seller. 
+The Seller has to return exactly the amount which the Buyer paid to purchase the item right before xx days. 
+This dispute is automatically closed.
+</p>
--- templates/resolution/case-details/info.php
+++ templates/resolution/case-details/info.php
@@ -9,66 +9,45 @@ me_print_notices();
         <div class="me-row">
             <div class="me-col-md-6">
                 <div class="me-disputed-order-info">
-                    <h3>
-                        <?php _e("Case infomation", "enginethemes");?>
-                    </h3>
+                    <h3><?php _e("Case infomation", "enginethemes");?></h3>
                     <p>
-                        <span>
-                            <?php _e("Case status:", "enginethemes");?>
-                        </span>
+                        <span><?php _e("Case status:", "enginethemes");?></span>
                         <?php echo me_dispute_status_label($case->post_status); ?>
                     </p>
                     <p>
-                        <span>
-                            <?php _e("Open date:", "enginethemes");?>
-                        </span>
-                        <?php echo date_i18n(get_option('date_format'), strtotime($case->post_date)); ?>
-                    </p>
+                        <span><?php _e("Open date:", "enginethemes");?></span>
+                        <?php echo date_i18n(get_option('date_format'), strtotime($case->post_date)); ?></p>
                     <p>
-                        <span>
-                            <?php _e("Listing:", "enginethemes");?>
-                        </span>
+                        <span><?php _e("Listing:", "enginethemes");?></span>
                         <a href="<?php echo get_permalink($item['ID']); ?>">
                             <?php echo $item['title']; ?>
                         </a>
                     </p>
                     <p>
-                        <span>
-                            <?php _e("Problem:", "enginethemes");?>
-                        </span>
+                        <span><?php _e("Problem:", "enginethemes");?></span>
                         <?php echo me_rc_dispute_problem_text($case->ID); ?>
                     </p>
                     <p>
-                        <span>
-                            <?php _e("You want to:", "enginethemes");?>
-                        </span>
+                        <span><?php _e("You want to:", "enginethemes");?></span>
                         <?php echo me_rc_case_expected_solution_label($case->ID); ?>
                     </p>
                 </div>
             </div>
             <div class="me-col-md-6">
                 <div class="me-disputed-order-info">
-                    <h3>
-                        <?php _e("Order information", "enginethemes");?>
-                    </h3>
+                    <h3><?php _e("Order information", "enginethemes");?></h3>
                     <p>
-                        <span>
-                            <?php _e("Order ID:", "enginethemes");?>
-                        </span>
+                        <span><?php _e("Order ID:", "enginethemes");?></span>
                         <a href="<?php echo $transaction->get_order_detail_url(); ?>">
                             #<?php echo $transaction->ID; ?>
                         </a>
                     </p>
                     <p>
-                        <span>
-                            <?php _e("Total amount:", "enginethemes");?>
-                        </span>
+                        <span><?php _e("Total amount:", "enginethemes");?></span>
                         <?php echo me_price_format($transaction->get_total()); ?>
                     </p>
                     <p>
-                        <span>
-                            <?php _e("Order date:", "enginethemes");?>
-                        </span>
+                        <span><?php _e("Order date:", "enginethemes");?></span>
                         <?php echo get_the_date(get_option('date_format'), $transaction->ID); ?>
                     </p>
                 </div>
@@ -95,7 +74,7 @@ me_print_notices();
             }
 
             if($case->post_status == 'me-resolved') {
-                me_get_template('resolution/case-details/case-open', array('case' => $case));
+                me_get_template('resolution/case-details/case-resolved', array('case' => $case));
             }
 
             ?>
