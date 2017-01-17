977f074be6d097c900a92d1357aac4ad1fad2ae3
dakachi <david87dang@gmail.com>
2017-01-17 17:44:10 +0700

    update me order

--- includes/class-me-order.php
+++ includes/class-me-order.php
@@ -147,15 +147,11 @@ class ME_Order
         } else {
             $post = $order_id;
         }
-<<<<<<< HEAD
 
         if (!$post || $post->post_type != 'me_order') {
             return false;
         }
-=======
-        
-        if(!$post || $post->post_type != 'me_order') return false;
->>>>>>> 1.0.1
+
 
         foreach (get_object_vars($post) as $key => $value) {
             $this->$key = $value;
