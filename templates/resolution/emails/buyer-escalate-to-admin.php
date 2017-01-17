284ba7fe659bf463a276075337d6a60a87cbaa2a
dakachi <david87dang@gmail.com>
2017-01-17 13:51:16 +0700

    update email template send to admin when user escalate

--- templates/resolution/emails/buyer-escalate-to-admin.php
+++ templates/resolution/emails/buyer-escalate-to-admin.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This is an email template send to buyer when seller escalate dispute
+ * This is an email template send to admin when buyer escalate dispute
  *
  * You can modify the email by copy it to {your theme}/templates/resolution/email
  * @since 1.1
@@ -14,7 +14,7 @@ if (!defined('ABSPATH')) {
 ?>
 <p><?php printf(__("Hi %s,", "enginethemes"), $display_name);?></p>
 <p>
-<?php printf(__("%s has escalated the dispute for the transaction: <a href='%s' >#%d</a>. ", "enginethemes"), $sender_name, $order_link, $order_id); ?>
+<?php printf(__("Buyer %s has escalated the dispute for the transaction: <a href='%s' >#%d</a>. ", "enginethemes"), $sender_name, $order_link, $order_id); ?>
 <?php printf(__("Please review it <a href='%s'>here</a> and arbitrate the dispute based on the detailed information and materials involved in this transaction which are provided by both parties.", "enginethemes"), $dispute_link); ?>
 </p>
 <p><?php printf(__("Regards, <br/> %s", "enginethemes"), $blogname);?></p>
